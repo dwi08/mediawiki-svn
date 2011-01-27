dwi08/mediawiki-svn

commit 6af2ba7527f43032892f7252c039c3555264141b

* Fixed a bug causing the installer to ignore the "engine" and "charset" settings when installing a MySQL database. * Fixed a bug causing the engine and charset settings to not be properly preserved when adding new tables on upgrade. * Fixed total breakage of SQLite upgrade, by reusing the administrative connection to the SQLite database instead of creating a new one when wfGetDB() is called. Added LBFactory_Single to support this. * Introduced a "schema variable" concept to DatabaseBase to avoid the use of globals for communication between the installer and the Database. Removed a lot of old global variable names from Database::replaceVars(), most were only added on a whim and were never used. * Introduced DatabaseInstaller::getSchemaVars(), to allow schema variables to be supplied by the DatabaseInstaller child classes. * Removed messages config-mysql-egine-mismatch [sic] and config-mysql-charset-mismatch. In the old installer it was possible for users to request a certain character set for an upgrade, but in the new installer the question is never asked. So these warnings were shown whenever a non-default character set or engine was used in the old database. * In MysqlInstaller::preUpgrade(), fixed the incorrect strings used to identify the MySQL character sets: mysql5 instead of utf8 and mysql5-binary instead of binary. * On install, initialise the site_stats table, using code copied from the old installer. Unlike the old installer, use SiteStats to increment the user count when the initial user is added. * Fixed several instances of inappropriate call-by-reference. * Replaced call_user_func_array() with call_user_func() where possible, it is shorter and simpler. * Moved the caching boilerplate for DatabaseInstaller::getConnection() to the base class, and have the derived classes override an uncached function openConnection() instead. Updates r80892. * In MysqlInstaller::getLocalSettings(), escape PHP strings correctly with LocalSettingsGenerator::escapePhpStrin

includes/db/Database.php
@@ -225,6 +225,7 @@ abstract class DatabaseBase implements DatabaseType {
 	protected $mLBInfo = array();
 	protected $mFakeSlaveLag = null, $mFakeMaster = false;
 	protected $mDefaultBigSelects = null;
+	protected $mSchemaVars = false;
 
 # ------------------------------------------------------------------------------
 # Accessors
@@ -2473,6 +2474,17 @@ abstract class DatabaseBase implements DatabaseType {
 	}
 
 	/**
+	 * Set variables to be used in sourceFile/sourceStream, in preference to the
+	 * ones in $GLOBALS. If an array is set here, $GLOBALS will not be used at 
+	 * all. If it's set to false, $GLOBALS will be used.
+	 *
+	 * @param $vars False, or array mapping variable name to value.
+	 */
+	function setSchemaVars( $vars ) {
+		$this->mSchemaVars = $vars;
+	}
+
+	/**
 	 * Read and execute commands from an open file handle
 	 * Returns true on success, error string or exception on failure (depending on object's error ignore settings)
 	 * @param $fp String: File handle
@@ -2547,8 +2559,8 @@ abstract class DatabaseBase implements DatabaseType {
 	}
 
 	/**
-	 * Database independent variable replacement, replaces a set of named variables
-	 * in a sql statement with the contents of their global variables.
+	 * Database independent variable replacement, replaces a set of variables
+	 * in a sql statement with their contents as given by $this->getSchemaVars().
 	 * Supports '{$var}' `{$var}` and / *$var* / (without the spaces) style variables
 	 * 
 	 * '{$var}' should be used for text and is passed through the database's addQuotes method
@@ -2558,16 +2570,17 @@ abstract class DatabaseBase implements DatabaseType {
 	 * / *$var* / is just encoded, besides traditional dbprefix and tableoptions it's use should be avoided
 	 * 
 	 * @param $ins String: SQL statement to replace variables in
-	 * @param $varnames Array: Array of global variable names to replace
 	 * @return String The new SQL statement with variables replaced
 	 */
-	protected function replaceGlobalVars( $ins, $varnames ) {
-		foreach ( $varnames as $var ) {
-			if ( isset( $GLOBALS[$var] ) ) {
-				$ins = str_replace( '\'{$' . $var . '}\'', $this->addQuotes( $GLOBALS[$var] ), $ins ); // replace '{$var}'
-				$ins = str_replace( '`{$' . $var . '}`', $this->addIdentifierQuotes( $GLOBALS[$var] ), $ins ); // replace `{$var}`
-				$ins = str_replace( '/*$' . $var . '*/', $this->strencode( $GLOBALS[$var] ) , $ins ); // replace /*$var*/
-			}
+	protected function replaceSchemaVars( $ins ) {
+		$vars = $this->getSchemaVars();
+		foreach ( $vars as $var => $value ) {
+			// replace '{$var}'
+			$ins = str_replace( '\'{$' . $var . '}\'', $this->addQuotes( $value ), $ins );
+			// replace `{$var}`
+			$ins = str_replace( '`{$' . $var . '}`', $this->addIdentifierQuotes( $value ), $ins );
+			// replace /*$var*/
+			$ins = str_replace( '/*$' . $var . '*/', $this->strencode( $value ) , $ins );
 		}
 		return $ins;
 	}
@@ -2576,13 +2589,7 @@ abstract class DatabaseBase implements DatabaseType {
 	 * Replace variables in sourced SQL
 	 */
 	protected function replaceVars( $ins ) {
-		$varnames = array(
-			'wgDBserver', 'wgDBname', 'wgDBintlname', 'wgDBuser',
-			'wgDBpassword', 'wgDBsqluser', 'wgDBsqlpassword',
-			'wgDBadminuser', 'wgDBadminpassword', 'wgDBTableOptions',
-		);
-
-		$ins = $this->replaceGlobalVars( $ins, $varnames );
+		$ins = $this->replaceSchemaVars( $ins );
 
 		// Table prefixes
 		$ins = preg_replace_callback( '!/\*(?:\$wgDBprefix|_)\*/([a-zA-Z_0-9]*)!',
@@ -2593,6 +2600,27 @@ abstract class DatabaseBase implements DatabaseType {
 			array( $this, 'indexNameCallback' ), $ins );
 
 		return $ins;
+	}
+
+	/**
+	 * Get schema variables. If none have been set via setSchemaVars(), then
+	 * use some defaults from the current object.
+	 */
+	protected function getSchemaVars() {
+		if ( $this->mSchemaVars ) {
+			return $this->mSchemaVars;
+		} else {
+			return $this->getDefaultSchemaVars();
+		}
+	}
+
+	/**
+	 * Get schema variables to use if none have been set via setSchemaVars().
+	 * Override this in derived classes to provide variables for tables.sql 
+	 * and SQL patch files. 
+	 */
+	protected function getDefaultSchemaVars() {
+		return array();
 	}
 
 	/**

includes/db/DatabaseOracle.php
@@ -1331,18 +1331,6 @@ class DatabaseOracle extends DatabaseBase {
 		return $this->mServer;
 	}
 
-	public function replaceVars( $ins ) {
-		$varnames = array( 'wgDBprefix' );
-		if ( $this->mFlags & DBO_SYSDBA ) {
-			$varnames[] = '_OracleDefTS';
-			$varnames[] = '_OracleTempTS';
-		}
-
-		$ins = $this->replaceGlobalVars( $ins, $varnames );
-
-		return parent::replaceVars( $ins );
-	}
-
 	public function getSearchEngine() {
 		return 'SearchOracle';
 	}

includes/installer/DatabaseInstaller.php
@@ -95,14 +95,14 @@ abstract class DatabaseInstaller {
 	}
 
 	/**
-	 * Connect to the database using the administrative user/password currently
-	 * defined in the session. On success, return the connection, on failure,
-	 *
-	 * This may be called multiple times, so the result should be cached.
+	 * Open a connection to the database using the administrative user/password
+	 * currently defined in the session, without any caching. Returns a status 
+	 * object. On success, the status object will contain a Database object in
+	 * its value member.
 	 *
 	 * @return Status
 	 */
-	public abstract function getConnection();
+	public abstract function openConnection();
 
 	/**
 	 * Create the database and return a Status object indicating success or
@@ -111,6 +111,29 @@ abstract class DatabaseInstaller {
 	 * @return Status
 	 */
 	public abstract function setupDatabase();
+
+	/**
+	 * Connect to the database using the administrative user/password currently
+	 * defined in the session. Returns a status object. On success, the status 
+	 * object will contain a Database object in its value member.
+	 * 
+	 * This will return a cached connection if one is available.
+	 *
+	 * @return DatabaseBase
+	 */
+	public function getConnection() {
+		if ( $this->db ) {
+			return Status::newGood( $this->db );
+		}
+		$status = $this->openConnection();
+		if ( $status->isOK() ) {
+			$this->db = $status->value;
+			// Enable autocommit
+			$this->db->clearFlag( DBO_TRX );
+			$this->db->commit();
+		}
+		return $status;
+	}
 
 	/**
 	 * Create database tables from scratch.
@@ -142,7 +165,7 @@ abstract class DatabaseInstaller {
 		}
 		// Resume normal operations
 		if( $status->isOk() ) {
-			LBFactory::enableBackend();
+			$this->enableLB();
 		}
 		return $status;
 	}
@@ -155,13 +178,56 @@ abstract class DatabaseInstaller {
 	public abstract function getLocalSettings();
 
 	/**
+	 * Override this to provide DBMS-specific schema variables, to be 
+	 * substituted into tables.sql and other schema files.
+	 */
+	public function getSchemaVars() {
+		return array();
+	}
+
+	/**
+	 * Set appropriate schema variables in the current database connection.
+	 *
+	 * This should be called after any request data has been imported, but before
+	 * any write operations to the database.
+	 */
+	public function setupSchemaVars() {
+		$status = $this->getConnection();
+		if ( $status->isOK() ) {
+			$status->value->setSchemaVars( $this->getSchemaVars() );
+		}
+	}
+
+	/**
+	 * Set up LBFactory so that wfGetDB() etc. works.
+	 * We set up a special LBFactory instance which returns the current 
+	 * installer connection.
+	 */
+	public function enableLB() {
+		$status = $this->getConnection();
+		if ( !$status->isOK() ) {
+			throw new MWException( __METHOD__.': unexpected DB connection error' );
+		}
+		LBFactory::setInstance( new LBFactory_Single( array(
+			'connection' => $status->value ) ) );
+	}
+
+	/**
+	 * Get a Database connection object. Throw an exception if we can't get one.
+	 *
+	 * @return DatabaseBase
+	 */
+	public function getConnectionOrDie() {
+	}
+
+	/**
 	 * Perform database upgrades
 	 *
 	 * @return Boolean
 	 */
 	public function doUpgrade() {
-		# Maintenance scripts like wfGetDB()
-		LBFactory::enableBackend();
+		$this->setupSchemaVars();
+		$this->enableLB();
 
 		$ret = true;
 		ob_start( array( $this, 'outputHandler' ) );
@@ -198,16 +264,6 @@ abstract class DatabaseInstaller {
 	 */
 	public function getGlobalNames() {
 		return $this->globalNames;
-	}
-
-	/**
-	 * Return any table options to be applied to all tables that don't
-	 * override them.
-	 *
-	 * @return Array
-	 */
-	public function getTableOptions() {
-		return array();
 	}
 
 	/**

includes/installer/Installer.php
@@ -556,6 +556,27 @@ abstract class Installer {
 	}
 
 	/**
+	 * Install step which adds a row to the site_stats table with appropriate 
+	 * initial values.
+	 */
+	public function populateSiteStats( DatabaseInstaller $installer ) {
+		$status = $installer->getConnection();
+		if ( !$status->isOK() ) {
+			return $status;
+		}
+		$status->value->insert( 'site_stats', array( 
+			'ss_row_id' => 1,
+			'ss_total_views' => 0,
+			'ss_total_edits' => 0,
+			'ss_good_articles' => 0,
+			'ss_total_pages' => 0,
+			'ss_users' => 0,
+			'ss_admins' => 0,
+			'ss_images' => 0 ) );
+		return Status::newGood();
+	}
+
+	/**
 	 * Exports all wg* variables stored by the installer into global scope.
 	 */
 	public function exportVars() {
@@ -1098,7 +1119,7 @@ abstract class Installer {
 					break;
 				}
 
-				$text = Http::get( $url . $file );
+				$text = Http::get( $url . $file, array( 'timeout' => 3 ) );
 				unlink( $dir . $file );
 
 				if ( $text == 'exec' ) {
@@ -1202,11 +1223,12 @@ abstract class Installer {
 	 * @param $installer DatabaseInstaller so we can make callbacks
 	 * @return array
 	 */
-	protected function getInstallSteps( DatabaseInstaller &$installer ) {
+	protected function getInstallSteps( DatabaseInstaller $installer ) {
 		$coreInstallSteps = array(
 			array( 'name' => 'database',   'callback' => array( $installer, 'setupDatabase' ) ),
 			array( 'name' => 'tables',     'callback' => array( $installer, 'createTables' ) ),
 			array( 'name' => 'interwiki',  'callback' => array( $installer, 'populateInterwikiTable' ) ),
+			array( 'name' => 'stats',      'callback' => array( $this, 'populateSiteStats' ) ),
 			array( 'name' => 'secretkey',  'callback' => array( $this, 'generateSecretKey' ) ),
 			array( 'name' => 'upgradekey', 'callback' => array( $this, 'generateUpgradeKey' ) ),
 			array( 'name' => 'sysop',      'callback' => array( $this, 'createSysop' ) ),
@@ -1254,16 +1276,17 @@ abstract class Installer {
 		$installResults = array();
 		$installer = $this->getDBInstaller();
 		$installer->preInstall();
+		$installer->setupSchemaVars();
 		$steps = $this->getInstallSteps( $installer );
 		foreach( $steps as $stepObj ) {
 			$name = $stepObj['name'];
 			call_user_func_array( $startCB, array( $name ) );
 
 			// Perform the callback step
-			$status = call_user_func_array( $stepObj['callback'], array( &$installer ) );
+			$status = call_user_func( $stepObj['callback'], $installer );
 
 			// Output and save the results
-			call_user_func_array( $endCB, array( $name, $status ) );
+			call_user_func( $endCB, $name, $status );
 			$installResults[$name] = $status;
 
 			// If we've hit some sort of fatal, we need to bail.
@@ -1364,6 +1387,10 @@ abstract class Installer {
 				$user->setEmail( $this->getVar( '_AdminEmail' ) );
 			}
 			$user->saveSettings();
+
+			// Update user count
+			$ssUpdate = new SiteStatsUpdate( 0, 0, 0, 0, 1 );
+			$ssUpdate->doUpdate();
 		}
 		$status = Status::newGood();
 
@@ -1400,7 +1427,7 @@ abstract class Installer {
 	 *
 	 * @return Status
 	 */
-	protected function createMainpage( DatabaseInstaller &$installer ) {
+	protected function createMainpage( DatabaseInstaller $installer ) {
 		$status = Status::newGood();
 		try {
 			// STUPID STUPID $wgTitle. PST calls getUserSig(), which joyfully

includes/installer/MysqlInstaller.php
@@ -111,25 +111,21 @@ class MysqlInstaller extends DatabaseInstaller {
 		return $status;
 	}
 
-	public function getConnection() {
+	public function openConnection() {
 		$status = Status::newGood();
-		if( is_null( $this->db ) ) {
-			try {
-				$this->db = new DatabaseMysql(
-					$this->getVar( 'wgDBserver' ),
-					$this->getVar( '_InstallUser' ),
-					$this->getVar( '_InstallPassword' ),
-					false,
-					false,
-					0,
-					$this->getVar( 'wgDBprefix' )
-				);
-				$status->value = $this->db;
-			} catch ( DBConnectionError $e ) {
-				$status->fatal( 'config-connection-error', $e->getMessage() );
-			}
-		} else {
-			$status->value = $this->db;
+		try {
+			$db = new DatabaseMysql(
+				$this->getVar( 'wgDBserver' ),
+				$this->getVar( '_InstallUser' ),
+				$this->getVar( '_InstallPassword' ),
+				false,
+				false,
+				0,
+				$this->getVar( 'wgDBprefix' )
+			);
+			$status->value = $db;
+		} catch ( DBConnectionError $e ) {
+			$status->fatal( 'config-connection-error', $e->getMessage() );
 		}
 		return $status;
 	}
@@ -158,9 +154,9 @@ class MysqlInstaller extends DatabaseInstaller {
 				if ( preg_match( '/^latin1/', $row->Collation ) ) {
 					$existingSchema = 'mysql4';
 				} elseif ( preg_match( '/^utf8/', $row->Collation ) ) {
-					$existingSchema = 'mysql5';
+					$existingSchema = 'utf8';
 				} elseif ( preg_match( '/^binary/', $row->Collation ) ) {
-					$existingSchema = 'mysql5-binary';
+					$existingSchema = 'binary';
 				} else {
 					$existingSchema = false;
 					$this->parent->showMessage( 'config-unknown-collation' );
@@ -177,11 +173,9 @@ class MysqlInstaller extends DatabaseInstaller {
 		}
 
 		if ( $existingSchema && $existingSchema != $this->getVar( '_MysqlCharset' ) ) {
-			$this->parent->showMessage( 'config-mysql-charset-mismatch', $this->getVar( '_MysqlCharset' ), $existingSchema );
 			$this->setVar( '_MysqlCharset', $existingSchema );
 		}
 		if ( $existingEngine && $existingEngine != $this->getVar( '_MysqlEngine' ) ) {
-			$this->parent->showMessage( 'config-mysql-egine-mismatch', $this->getVar( '_MysqlEngine' ), $existingEngine );
 			$this->setVar( '_MysqlEngine', $existingEngine );
 		}
 
@@ -445,16 +439,36 @@ class MysqlInstaller extends DatabaseInstaller {
 		return $status;
 	}
 
-	public function getTableOptions() {
-		return array( 'engine' => $this->getVar( '_MysqlEngine' ),
-			'default charset' => $this->getVar( '_MysqlCharset' ) );
+	/**
+	 * Return any table options to be applied to all tables that don't
+	 * override them.
+	 *
+	 * @return String
+	 */
+	protected function getTableOptions() {
+		$options = array();
+		if ( $this->getVar( '_MysqlEngine' ) !== null ) {
+			$options[] = "ENGINE=" . $this->getVar( '_MysqlEngine' );
+		}
+		if ( $this->getVar( '_MysqlCharset' ) !== null ) {
+			$options[] = 'DEFAULT CHARSET=' . $this->getVar( '_MysqlCharset' );
+		}
+		return implode( ', ', $options );
+	}
+
+	/**
+	 * Get variables to substitute into tables.sql and the SQL patch files.
+	 */
+	public function getSchemaVars() {
+		return array(
+			'wgDBTableOptions' => $this->getTableOptions(),
+		);
 	}
 
 	public function getLocalSettings() {
 		$dbmysql5 = wfBoolToStr( $this->getVar( 'wgDBmysql5', true ) );
-		$prefix = $this->getVar( 'wgDBprefix' );
-		$opts = $this->getTableOptions();
-		$tblOpts = "ENGINE=" . $opts['engine'] . ', DEFAULT CHARSET=' . $opts['default charset'];
+		$prefix = LocalSettingsGenerator::escapePhpString( $this->getVar( 'wgDBprefix' ) );
+		$tblOpts = LocalSettingsGenerator::escapePhpString( $this->getTableOptions() );
 		return
 "# MySQL specific settings
 \$wgDBprefix         = \"{$prefix}\";

includes/installer/OracleInstaller.php
@@ -109,11 +109,11 @@ class OracleInstaller extends DatabaseInstaller {
 		return $status;
 	}
 
-	public function getConnection() {
+	public function openConnection() {
 		$status = Status::newGood();
 		try {
 			if ( $this->useSysDBA ) {
-				$this->db = new DatabaseOracle(
+				$db = new DatabaseOracle(
 					$this->getVar( 'wgDBserver' ),
 					$this->getVar( '_InstallUser' ),
 					$this->getVar( '_InstallPassword' ),
@@ -122,7 +122,7 @@ class OracleInstaller extends DatabaseInstaller {
 					$this->getVar( 'wgDBprefix' )
 				);
 			} else {
-				$this->db = new DatabaseOracle(
+				$db = new DatabaseOracle(
 					$this->getVar( 'wgDBserver' ),
 					$this->getVar( 'wgDBuser' ),
 					$this->getVar( 'wgDBpassword' ),
@@ -131,7 +131,7 @@ class OracleInstaller extends DatabaseInstaller {
 					$this->getVar( 'wgDBprefix' )
 				);
 			}
-			$status->value = $this->db;
+			$status->value = $db;
 		} catch ( DBConnectionError $e ) {
 			$status->fatal( 'config-connection-error', $e->getMessage() );
 		}
@@ -176,12 +176,6 @@ class OracleInstaller extends DatabaseInstaller {
 		}
 
 		if ( !$this->db->selectDB( $this->getVar( 'wgDBuser' ) ) ) {
-			/**
-			 * The variables $_OracleDefTS, $_OracleTempTS are used by maintenance/oracle/user.sql
-			 * Set here for fetching in DatabaseOracle::replaceVars()
-			 */
-			$GLOBALS['_OracleDefTS'] = $this->getVar( '_OracleDefTS' );
-			$GLOBALS['_OracleTempTS'] = $this->getVar( '_OracleTempTS' );
 			$this->db->setFlag( DBO_DDLMODE );
 			$error = $this->db->sourceFile( "$IP/maintenance/oracle/user.sql" );
 			if ( $error !== true || !$this->db->selectDB( $this->getVar( 'wgDBuser' ) ) ) {
@@ -203,6 +197,15 @@ class OracleInstaller extends DatabaseInstaller {
 		return $status;
 	}
 
+	public function getSchemaVars() {
+		/**
+		 * The variables $_OracleDefTS, $_OracleTempTS are used by maintenance/oracle/user.sql
+		 */
+		return array(
+			'_OracleDefTS' => $this->getVar( '_OracleDefTS' ),
+			'_OracleTempTS' => $this->getVar( '_OracleTempTS' ),
+		);
+	}
 
 	public function getLocalSettings() {
 		$prefix = $this->getVar( 'wgDBprefix' );

includes/installer/PostgresInstaller.php
@@ -98,21 +98,17 @@ class PostgresInstaller extends DatabaseInstaller {
 		return $status;
 	}
 
-	function getConnection($database = 'template1') {
+	function openConnection( $database = 'template1' ) {
 		$status = Status::newGood();
-		if( is_null( $this->db ) ) {
-			try {
-				$this->db = new DatabasePostgres(
-					$this->getVar( 'wgDBserver' ),
-					$this->getVar( '_InstallUser' ),
-					$this->getVar( '_InstallPassword' ),
-					$database );
-				$status->value = $this->db;
-			} catch ( DBConnectionError $e ) {
-				$status->fatal( 'config-connection-error', $e->getMessage() );
-			}
-		} else {
-			$status->value = $this->db;
+		try {
+			$db = new DatabasePostgres(
+				$this->getVar( 'wgDBserver' ),
+				$this->getVar( '_InstallUser' ),
+				$this->getVar( '_InstallPassword' ),
+				$database );
+			$status->value = $db;
+		} catch ( DBConnectionError $e ) {
+			$status->fatal( 'config-connection-error', $e->getMessage() );
 		}
 		return $status;
 	}

commit 05961e35454f1a497f18dd795b402e494cfddbb1

* Fixed Oracle new installer support, broken by r80957. This is a minimal patch and doesn't address the architectural issues. ** Moved the responsibility for calling setupSchemaVars() on install to the DatabaseInstaller subclass. This allows it to be called after setupDatabase() has completed, as required by Oracle and PostgreSQL. ** Fixed OracleInstaller::getConnection() so that it respects $this->useSysDBA correctly. ** In OracleInstaller, added some more variables to the list of schema vars, which are needed by user.sql and tables.sql ** In SearchOracle, specify the database name when calling ctx_ddl.sync_index(). This fixes a fatal error in the createMainpage step, caused by the schema name not being equal to the current user.
* In oracle/tables.sql, fixed a couple of indexes with missing table prefixes.
* Improved debugging output in DatabaseInstaller::getConnection() and Installer::createMainpage().
* In DatabaseBase::selectDB(), set $this->mDBname correctly, as in DatabaseMysql.



git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@81084 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/db/Database.php
@@ -1474,6 +1474,7 @@ abstract class DatabaseBase implements DatabaseType {
 		# Stub.  Shouldn't cause serious problems if it's not overridden, but
 		# if your database engine supports a concept similar to MySQL's
 		# databases you may as well.
+		$this->mDBname = $db;
 		return true;
 	}
 

includes/db/DatabaseOracle.php
@@ -1086,10 +1086,16 @@ class DatabaseOracle extends DatabaseBase {
 	}
 
 	function selectDB( $db ) {
-		if ( $db == null || $db == $this->mUser ) { return true; }
+		$this->mDBname = $db;
+		if ( $db == null || $db == $this->mUser ) {
+			return true;
+		}
 		$sql = 'ALTER SESSION SET CURRENT_SCHEMA=' . strtoupper($db);
 		$stmt = oci_parse( $this->mConn, $sql );
-		if ( !oci_execute( $stmt ) ) {
+		wfSuppressWarnings();
+		$success = oci_execute( $stmt );
+		wfRestoreWarnings();
+		if ( !$success ) {
 			$e = oci_error( $stmt );
 			if ( $e['code'] != '1435' ) {
 				$this->reportQueryError( $e['message'], $e['code'], $sql, __METHOD__ );

includes/installer/DatabaseInstaller.php
@@ -195,6 +195,8 @@ abstract class DatabaseInstaller {
 		$status = $this->getConnection();
 		if ( $status->isOK() ) {
 			$status->value->setSchemaVars( $this->getSchemaVars() );
+		} else {
+			throw new MWException( __METHOD__.': unexpected DB connection error' );
 		}
 	}
 

includes/installer/Installer.i18n.php
@@ -471,7 +471,7 @@ Consider changing it manually.",
 	'config-install-sysop'            => 'Creating administrator user account',
 	'config-install-subscribe-fail'   => 'Unable to subscribe to mediawiki-announce',
 	'config-install-mainpage'         => 'Creating main page with default content',
-	'config-install-mainpage-failed'  => 'Could not insert main page.',
+	'config-install-mainpage-failed'  => 'Could not insert main page: $1',
 	'config-install-done'             => "'''Congratulations!'''
 You have successfully installed MediaWiki.
 

includes/installer/Installer.php
@@ -1272,7 +1272,6 @@ abstract class Installer {
 		$installResults = array();
 		$installer = $this->getDBInstaller();
 		$installer->preInstall();
-		$installer->setupSchemaVars();
 		$steps = $this->getInstallSteps( $installer );
 		foreach( $steps as $stepObj ) {
 			$name = $stepObj['name'];

includes/installer/MysqlInstaller.php
@@ -414,6 +414,7 @@ class MysqlInstaller extends DatabaseInstaller {
 			$conn->query( "CREATE DATABASE " . $conn->addIdentifierQuotes( $dbName ), __METHOD__ );
 			$conn->selectDB( $dbName );
 		}
+		$this->setupSchemaVars();
 		return $status;
 	}
 

includes/installer/OracleInstaller.php
@@ -31,6 +31,8 @@ class OracleInstaller extends DatabaseInstaller {
 
 	public $minimumVersion = '9.0.1'; // 9iR1
 
+	protected $sysConn, $userConn;
+
 	public function getName() {
 		return 'oracle';
 	}
@@ -117,7 +119,7 @@ class OracleInstaller extends DatabaseInstaller {
 					$this->getVar( 'wgDBserver' ),
 					$this->getVar( '_InstallUser' ),
 					$this->getVar( '_InstallPassword' ),
-					$this->getVar( 'wgDBname' ),
+					$this->getVar( '_InstallUser' ),
 					DBO_SYSDBA | DBO_DDLMODE,
 					$this->getVar( 'wgDBprefix' )
 				);
@@ -135,6 +137,42 @@ class OracleInstaller extends DatabaseInstaller {
 		} catch ( DBConnectionError $e ) {
 			$status->fatal( 'config-connection-error', $e->getMessage() );
 		}
+		return $status;
+	}
+
+	/**
+	 * Cache the two different types of connection which can be returned by 
+	 * openConnection().
+	 *
+	 * $this->db will be set to the last used connection object.
+	 *
+	 * FIXME: openConnection() should not be doing two different things.
+	 */
+	public function getConnection() {
+		// Check cache
+		if ( $this->useSysDBA ) {
+			$conn = $this->sysConn;
+		} else {
+			$conn = $this->userConn;
+		}
+		if ( $conn !== null ) {
+			$this->db = $conn;
+			return Status::newGood( $conn );
+		}
+
+		// Open a new connection
+		$status = $this->openConnection();
+		if ( !$status->isOK() ) {
+			return $status;
+		}
+
+		// Save to the cache
+		if ( $this->useSysDBA ) {
+			$this->sysConn = $status->value;
+		} else {
+			$this->userConn = $status->value;
+		}
+		$this->db = $status->value;
 		return $status;
 	}
 
@@ -175,6 +213,8 @@ class OracleInstaller extends DatabaseInstaller {
 			return $status;
 		}
 
+		$this->setupSchemaVars();
+
 		if ( !$this->db->selectDB( $this->getVar( 'wgDBuser' ) ) ) {
 			$this->db->setFlag( DBO_DDLMODE );
 			$error = $this->db->sourceFile( "$IP/maintenance/oracle/user.sql" );
@@ -183,6 +223,7 @@ class OracleInstaller extends DatabaseInstaller {
 			}
 		}
 
+
 		return $status;
 	}
 
@@ -190,6 +231,8 @@ class OracleInstaller extends DatabaseInstaller {
 	 * Overload: after this action field info table has to be rebuilt
 	 */
 	public function createTables() {
+		$this->setupSchemaVars();
+		$this->db->selectDB( $this->getVar( 'wgDBuser' ) );
 		$status = parent::createTables();
 
 		$this->db->query( 'BEGIN fill_wiki_info; END;' );
@@ -198,13 +241,21 @@ class OracleInstaller extends DatabaseInstaller {
 	}
 
 	public function getSchemaVars() {
-		/**
-		 * The variables $_OracleDefTS, $_OracleTempTS are used by maintenance/oracle/user.sql
-		 */
-		return array(
-			'_OracleDefTS' => $this->getVar( '_OracleDefTS' ),
-			'_OracleTempTS' => $this->getVar( '_OracleTempTS' ),
+		$varNames = array(
+			# These variables are used by maintenance/oracle/user.sql
+			'_OracleDefTS',
+			'_OracleTempTS',
+			'wgDBuser',
+			'wgDBpassword',
+
+			# These are used by tables.sql
+			'wgDBprefix',
 		);
+		$vars = array();
+		foreach ( $varNames as $name ) {
+			$vars[$name] = $this->getVar( $name );
+		}
+		return $vars;
 	}
 
 	public function getLocalSettings() {

includes/installer/PostgresInstaller.php
@@ -215,6 +215,7 @@ class PostgresInstaller extends DatabaseInstaller {
 		if ( !$status->isOK() ) {
 			return $status;
 		}
+		$this->setupSchemaVars();
 		$conn = $status->value;
 
 		$dbName = $this->getVar( 'wgDBname' );

includes/installer/SqliteInstaller.php
@@ -144,6 +144,7 @@ class SqliteInstaller extends DatabaseInstaller {
 		$this->setVar( 'wgDBserver', '' );
 		$this->setVar( 'wgDBuser', '' );
 		$this->setVar( 'wgDBpassword', '' );
+		$this->setupSchemaVars();
 		return $this->getConnection();
 	}
 

includes/search/SearchOracle.php
@@ -246,8 +246,16 @@ class SearchOracle extends SearchEngine {
 				'si_title' => $title,
 				'si_text' => $text
 			), 'SearchOracle::update' );
-		$dbw->query("CALL ctx_ddl.sync_index('si_text_idx')");
-		$dbw->query("CALL ctx_ddl.sync_index('si_title_idx')");
+
+		// Sync the index
+		// We need to specify the DB name (i.e. user/schema) here so that 
+		// it can work from the installer, where
+		//     ALTER SESSION SET CURRENT_SCHEMA = ...
+		// was used.
+		$dbw->query( "CALL ctx_ddl.sync_index(" . 
+			$dbw->addQuotes( $dbw->getDBname() . '.si_text_idx' ) . ")" );
+		$dbw->query( "CALL ctx_ddl.sync_index(" . 
+			$dbw->addQuotes( $dbw->getDBname() . '.si_title_idx' ) . ")" );
 	}
 
 	/**

maintenance/oracle/tables.sql
@@ -610,8 +610,8 @@ ALTER TABLE &mw_prefix.valid_tag ADD CONSTRAINT &mw_prefix.valid_tag_pk PRIMARY 
 --);
 --CREATE UNIQUE INDEX &mw_prefix.profiling_u01 ON &mw_prefix.profiling (pf_name, pf_server);
 
-CREATE INDEX si_title_idx ON &mw_prefix.searchindex(si_title) INDEXTYPE IS ctxsys.context;
-CREATE INDEX si_text_idx ON &mw_prefix.searchindex(si_text) INDEXTYPE IS ctxsys.context;
+CREATE INDEX &mw_prefix.si_title_idx ON &mw_prefix.searchindex(si_title) INDEXTYPE IS ctxsys.context;
+CREATE INDEX &mw_prefix.si_text_idx ON &mw_prefix.searchindex(si_text) INDEXTYPE IS ctxsys.context;
 
 CREATE TABLE &mw_prefix.l10n_cache (
   lc_lang varchar2(32) NOT NULL,