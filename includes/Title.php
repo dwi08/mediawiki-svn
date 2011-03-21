dwi08/mediawiki-svn

commit c20f497ef99250f2cfbeaa909d41d719a35becc7

Move the file related parts of Title::isValidMoveOperation to its own function, Title::validateFileMoveOperation
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@83282 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/Title.php
@@ -2986,28 +2986,7 @@ class Title {
 		}
 
 		// Image-specific checks
-		if ( $this->getNamespace() == NS_FILE ) {
-			if ( $nt->getNamespace() != NS_FILE ) {
-				$errors[] = array( 'imagenocrossnamespace' );
-			}
-			$file = wfLocalFile( $this );
-			if ( $file->exists() ) {
-				if ( $nt->getText() != wfStripIllegalFilenameChars( $nt->getText() ) ) {
-					$errors[] = array( 'imageinvalidfilename' );
-				}
-				if ( !File::checkExtensionCompatibility( $file, $nt->getDBkey() ) ) {
-					$errors[] = array( 'imagetypemismatch' );
-				}
-			}
-			$destfile = wfLocalFile( $nt );
-			if ( !$wgUser->isAllowed( 'reupload-shared' ) && !$destfile->exists() && wfFindFile( $nt ) ) {
-				$errors[] = array( 'file-exists-sharedrepo' );
-			}
-		}
-
-		if ( $nt->getNamespace() == NS_FILE && $this->getNamespace() != NS_FILE ) {
-			$errors[] = array( 'nonfile-cannot-move-to-file' );
-		}
+		$errors = array_merge( $errors, $this->validateFileMoveOperation( $nt ) );
 
 		if ( $auth ) {
 			$errors = wfMergeErrorArrays( $errors,
@@ -3046,6 +3025,42 @@ class Title {
 		if ( empty( $errors ) ) {
 			return true;
 		}
+		return $errors;
+	}
+	
+	/**
+	 * Check if the requested move target is a valid file move target
+	 * @param Title $nt Target title
+	 * @return array List of errors
+	 */
+	protected function validateFileMoveOperation( $nt ) {
+		global $wgUser;
+		
+		$errors = array();
+		
+		if ( $nt->getNamespace() != NS_FILE ) {
+			$errors[] = array( 'imagenocrossnamespace' );
+		}
+		
+		$file = wfLocalFile( $this );
+		if ( $file->exists() ) {
+			if ( $nt->getText() != wfStripIllegalFilenameChars( $nt->getText() ) ) {
+				$errors[] = array( 'imageinvalidfilename' );
+			}
+			if ( !File::checkExtensionCompatibility( $file, $nt->getDBkey() ) ) {
+				$errors[] = array( 'imagetypemismatch' );
+			}
+		}
+		
+		$destFile = wfLocalFile( $nt );
+		if ( !$wgUser->isAllowed( 'reupload-shared' ) && !$destfile->exists() && wfFindFile( $nt ) ) {
+			$errors[] = array( 'file-exists-sharedrepo' );
+		}
+
+		if ( $nt->getNamespace() == NS_FILE && $this->getNamespace() != NS_FILE ) {
+			$errors[] = array( 'nonfile-cannot-move-to-file' );
+		}
+		
 		return $errors;
 	}
 

commit 039527fccf640ae96e811f3d2ce208a40e0f649e

Fix variable typo introduced in r83282
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@84489 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/Title.php
@@ -3054,7 +3054,7 @@ class Title {
 		}
 
 		$destFile = wfLocalFile( $nt );
-		if ( !$wgUser->isAllowed( 'reupload-shared' ) && !$destfile->exists() && wfFindFile( $nt ) ) {
+		if ( !$wgUser->isAllowed( 'reupload-shared' ) && !$destFile->exists() && wfFindFile( $nt ) ) {
 			$errors[] = array( 'file-exists-sharedrepo' );
 		}
 