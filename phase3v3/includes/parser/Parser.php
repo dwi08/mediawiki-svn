ae7096b554b3933a5ca932c82ecfb3df581bc7cb
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-07-24 17:44:46 +0000

    Revert r92990
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/iwtransclusion@92991 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phase3v3/includes/parser/Parser.php
+++ phase3v3/includes/parser/Parser.php
@@ -3051,7 +3051,7 @@ class Parser {
 	 * @private
 	 */
 	function braceSubstitution( $piece, $frame ) {
-		global $wgContLang, $wgNonincludableNamespaces, $wgEnableInterwikiTranscluding, $wgEnableInterwikiTemplatesTracking;
+		global $wgContLang, $wgNonincludableNamespaces;
 		wfProfileIn( __METHOD__ );
 		wfProfileIn( __METHOD__.'-setup' );
 
@@ -3266,14 +3266,10 @@ class Parser {
 					$text = "[[:$titleText]]";
 					$found = true;
 				}
-			} elseif ( $wgEnableInterwikiTranscluding && $title->isTrans() ) {
+			} elseif ( $title->isTrans() ) {
 				// TODO: Work by Peter17 in progress
 
 				$text = Interwiki::interwikiTransclude( $title );
-				
-				if ( $wgEnableInterwikiTemplatesTracking ) {
-					$this->registerDistantTemplate( $title );
-				}
 				
 				if ( $text !== false ) {
 					# Preprocess it like a template
@@ -3433,17 +3429,6 @@ class Parser {
 	 * @param Title $title
 	 * @return mixed string or false
 	 */
-	function registerDistantTemplate( $title ) {
-		$stuff = Parser::distantTemplateCallback( $title, $this );
-		$text = $stuff['text'];
-		$finalTitle = isset( $stuff['finalTitle'] ) ? $stuff['finalTitle'] : $title;
-		if ( isset( $stuff['deps'] ) ) {
-			foreach ( $stuff['deps'] as $dep ) {
-				$this->mOutput->addDistantTemplate( $dep['title'], $dep['page_id'], $dep['rev_id'] );
-			}
-		}
-	}
-
 	function fetchTemplate( $title ) {
 		$rv = $this->fetchTemplateAndTitle( $title );
 		return $rv[0];
