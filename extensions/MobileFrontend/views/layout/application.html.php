b9be34abd8329703c845ef201083e063ce797729
preilly <preilly@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-02 12:19:37 +0000

    mft r93733
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf/1.17wmf1@93734 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/MobileFrontend/views/layout/application.html.php
+++ extensions/MobileFrontend/views/layout/application.html.php
@@ -1,27 +1,32 @@
 <?php
-global $wgExtensionAssetsPath;
+global $wgExtensionAssetsPath, $wgAppleTouchIcon;
+
+if( $wgAppleTouchIcon !== false ) {
+	$appleTouchIconTag = Html::element( 'link', array( 'rel' => 'apple-touch-icon', 'href' => $wgAppleTouchIcon ) );
+} else {
+	$appleTouchIconTag = "";
+}
 
 $applicationHtml = <<<EOT
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
-<html lang='{$code}' dir='{$dir}' xml:lang='{$code}' xmlns='http://www.w3.org/1999/xhtml'> 
-  <head> 
-    <title>{$title}</title> 
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" /> 
-    <link href='{$wgExtensionAssetsPath}/MobileFrontend/stylesheets/{$cssFileName}.css' media='all' rel='Stylesheet' type='text/css' /> 
-    <meta name="ROBOTS" content="NOINDEX, NOFOLLOW" /> 
-    <meta name = "viewport" content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" /> 
-    <link rel="apple-touch-icon" href="http://en.m.wikipedia.org/apple-touch-icon.png" /> 
-    <script type='text/javascript'> 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang='{$code}' dir='{$dir}' xml:lang='{$code}' xmlns='http://www.w3.org/1999/xhtml'>
+  <head>
+    <title>{$htmlTitle}</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link href='{$wgExtensionAssetsPath}/MobileFrontend/stylesheets/{$cssFileName}.css' media='all' rel='Stylesheet' type='text/css' />
+    <meta name="ROBOTS" content="NOINDEX, NOFOLLOW" />
+    <meta name = "viewport" content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+    ${appleTouchIconTag}
+    <script type='text/javascript'>
       //<![CDATA[
         var title = "{$title}";
-        var server = "http://en.wikipedia.org";
         function shouldCache() {
           return true;
         }
       //]]>
-    </script> 
-    <script type="text/javascript" language="javascript" SRC="{$wgExtensionAssetsPath}/MobileFrontend/javascripts/jquery.js"></script> 
-    <script type="text/javascript" language="javascript" SRC="{$wgExtensionAssetsPath}/MobileFrontend/javascripts/application.js"></script> 
+    </script>
+    <script type="text/javascript" language="javascript" SRC="{$wgExtensionAssetsPath}/MobileFrontend/javascripts/jquery.js"></script>
+    <script type="text/javascript" language="javascript" SRC="{$wgExtensionAssetsPath}/MobileFrontend/javascripts/application.js"></script>
   </head>
   <body>
 	{$searchWebkitHtml}
