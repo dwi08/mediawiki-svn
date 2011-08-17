25f6ab26d51fbc89d89c2a8d0df7e182549862a8
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-08-17 22:33:54 +0000

    MFT r94690, r94829 with skeleton language file. LU should take care of the rest.
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf/1.17wmf1@94830 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ languages/messages/MessagesQug.php
@@ -0,0 +1,11 @@
+<?php
+/** Runa shimi (Runa shimi)
+ *
+ * See MessagesQqq.php for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ */
+
+$fallback = 'es';
