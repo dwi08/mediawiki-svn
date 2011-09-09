dwi08/mediawiki-svn

commit 99ecc44fc12b3bd4087c6631e2418c656553453b

[ResourceLoader2] Gadget manager (start of ajax editor) * Add column to SpecialGadgetManager front-end for last modified timestamp. * Use getTitleMessage. Logic is now centralized (yay r95970). * Expose gadget internal name to JavaScript via data- attribute. * Expose array of all rights to JavaScript via OutputPage's getJSVars hook. * Only load ajax editor if user is allowed to edit NS_GADGET_DEFINITION. * Introduce three resource modules: ** ext.gadgetmanager.api: Group of (partially stub) function to interact with the API. Getting gadget metadata, list of categories, save gadget, delete gadget etc. ** ext.gadgetmanager.ui: Binds event handlers to the table outputted by PHP and takes care of the form/dialog generation and the autocompletion of all input fields through various apis and arrays. ** jquery.createPropcloud: jQuery plugin written from scratch for the gadget manager. Turns an input field into a properties "cloud" with autocompletion and visual bubbles to remove items.
TODO:
* ext.gadgetmanager.api
** Finish stub functions for saving and deleting a gadget.
* ext.gadgetmanager.ui
** Nofication system
** Add "Create" tab to the special page, which just brings up a regular modify-form except that it will feature a title-input field.
** Add dynamic "(new category)" option in the category dropdown of the gadget editor.
* SpecialGadgetManager.php
** generateGadgetView


(Follows-up r95818)

git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/RL2@96309 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/Gadgets/modules/ext.gadgets.gadgetmanager.api.js
@@ -1,0 +1,187 @@
+/**
+ * Implement the editing API for the gadget manager.
+ *
+ * @author Timo Tijhof
+ * @copyright © 2011 Timo Tijhof
+ * @license GNU General Public Licence 2.0 or later
+ */
+( function() {
+
+	var
+		/**
+		 * @var {Object} Keyed by gadget id, contains the metadata as an object.
+		 */
+		gadgetCache = {},
+		/**
+		 * @var {Object|Null} If cache, object with category ids and the member counts */
+		gadgetCategoryCache = null;
+
+	/* Local functions */
+
+	/**
+	 * For most returns from api.* functions, a clone is made when data from
+	 * cached is used. This is to avoid situations where later modifications
+	 * (ie. by the ajax editor) on the object affecting the cache (because
+	 * the object would otherwise be passed by reference).
+	 */
+	function objClone( obj ) {
+		/**
+		 * A normal `$.extend( {}, obj );` is not suffecient,
+		 * it has to be recursive, because the values of this
+		 * object are also refererenes to objects.
+		 * Consider:
+		 * <code>
+		 *     var a = { words: [ 'foo', 'bar','baz' ] };
+		 *     var b = $.extend( {}, a );
+		 *     b.words.push( 'quux' );
+		 *     a.words[4]; // quux !
+		 * </code>
+		 */
+		 return $.extend( true /* resursive */, {}, obj );
+	}
+	function arrClone( arr ) {
+		return arr.slice();
+	}
+
+	/* Public functions */
+
+	mw.gadgetManager = {
+
+		conf: mw.config.get( 'gadgetManagerConf' ),
+
+		api: {
+
+			/**
+			 * Get gadget blob from the API (or from cache if available).
+			 *
+			 * @param id {String} Gadget id.
+			 * @param callback {Function} To be called with an object as first argument,
+			 * and status as second argument (success or error).
+			 * @return {jqXHR|Null}: Null if served from cache, otherwise the jqXHR.
+			 */
+			getGadgetMetadata: function( id, callback ) {
+				// Check cache
+				if ( id in gadgetCache && gadgetCache[id] !== null ) {
+					callback( objClone( gadgetCache[id] ), 'success' );
+					return null;
+				}
+				// Get from API if not cached
+				return $.ajax({
+					url: mw.util.wikiScript( 'api' ),
+					data: {
+						format: 'json',
+						action: 'query',
+						list: 'gadgets',
+						gaprop: 'name|metadata|desc',
+						ganames: id,
+						galanguage: mw.config.get( 'wgUserLanguage' )
+					},
+					type: 'GET',
+					dataType: 'json',
+					success: function( data ) {
+						if ( data && data.query && data.query.gadgets && data.query.gadgets[0] ) {
+							data = data.query.gadgets[0].metadata;
+							// Update cache
+							gadgetCache[id] = data;
+							callback( objClone( data ), 'success' );
+						} else {
+							// Invalidate cache
+							gadgetCache[id] = null;
+							callback( {}, 'error' );
+						}
+					},
+					error: function() {
+						// Invalidate cache
+						gadgetCache[id] = null;
+						callback( {}, 'error' );
+					}
+				});
+			},
+
+			/**
+			 * @param callback {Function} To be called with an array as first argument,
+			 * and status as second argument (success or error).
+			 * @return {jqXHR|Null}: Null if served from cache, otherwise the jqXHR.
+			 */
+			getGadgetCategories: function( callback ) {
+				// Check cache
+				if ( gadgetCategoryCache !== null ) {
+					callback( arrClone( gadgetCategoryCache ) );
+					return null;
+				}
+				// Get from API if not cached
+				return $.ajax({
+					url: mw.util.wikiScript( 'api' ),
+					data: {
+						format: 'json',
+						action: 'query',
+						list: 'gadgetcategories',
+						gcprop: 'name|title|members',
+						gclanguage: mw.config.get( 'wgUserLanguage' )
+					},
+					type: 'GET',
+					dataType: 'json',
+					success: function( data ) {
+						if ( data && data.query && data.query.gadgetcategories
+							&& data.query.gadgetcategories[0] )
+						{
+							data = data.query.gadgetcategories;
+							// Update cache
+							gadgetCategoryCache = data;
+							callback( arrClone( data ), 'success' );
+						} else {
+							// Invalidate cache
+							gadgetCategoryCache = null;
+							callback( [], 'error' );
+						}
+					},
+					error: function() {
+						// Invalidate cache
+						gadgetCategoryCache = null;
+						callback( [], 'error' );
+					}
+				});
+			},
+
+			/**
+			 * Creates or edits an existing gadget definition.
+			 *
+			 * @param gadget {Object}
+			 * - id {String} Id of the gadget to modify
+			 * - blob {Object} Gadget meta data
+			 * @param callback {Function} Called with two arguments:
+			 * - status ('ok' or 'error')
+			 * - msg (localized, something like "Successfull", "Conflict occurred" etc.)
+			 * @return {jqXHR|Null}: Null if served from cache, otherwise the jqXHR.
+			 */
+			doModifyGadget: function( gadget, callback ) {
+				mw.log( gadget );
+				// @todo
+				// Get token
+				// JSON.stringify
+				// Do with ApiEdit
+				// Invalidate cache
+				gadgetCache[gadget.id] = null;
+				callback( 'error', '@todo: Saving not implemented yet. Check console for object that would be saved.' );
+				return null;
+			},
+
+			/**
+			 * Deletes a gadget definition.
+			 *
+			 * @param id {String} Id of the gadget to delete.
+			 * @param callback {Function} Called with one argument (ok', 'error' or 'conflict').
+			 * @return {jqXHR|Null}: Null if served from cache, otherwise the jqXHR.
+			 */
+			doDeleteGadget: function( id, callback ) {
+				// @todo
+				// Do with ApiDelete
+				// Invalidate cache
+				gadgetCache[id] = null;
+				callback( 'error' );
+				return null;
+			}
+		}
+	};
+
+})();

extensions/Gadgets/modules/ext.gadgets.gadgetmanager.ui.js
@@ -1,0 +1,417 @@
+/**
+ * JavaScript to initialize the UI of the gadget manager.
+ *
+ * @author Timo Tijhof
+ * @copyright © 2011 Timo Tijhof
+ * @license GNU General Public Licence 2.0 or later
+ */
+( function() {
+
+	var
+		/**
+		 * @var {Object} Local alias to gadgetmananger
+		 */
+		gm = mw.gadgetManager,
+		/**
+		 * @var {Object} HTML fragements
+		 */
+		tpl = {
+			fancyForm: '<form class="mw-gadgetmanager-form">\
+					<fieldset>\
+						<legend>Module properties</legend>\
+						<table>\
+							<tr>\
+								<td><label for="mw-gadgetmanager-input-scripts"><html:msg key="gadgetmanager-prop-scripts"></label></td>\
+								<td><input type="text" id="mw-gadgetmanager-input-scripts" /></td>\
+							</tr>\
+							<tr>\
+								<td><label for="mw-gadgetmanager-input-styles"><html:msg key="gadgetmanager-prop-styles"></label></td>\
+								<td><input type="text" id="mw-gadgetmanager-input-styles" /></td>\
+							</tr>\
+							<tr>\
+								<td><label for="mw-gadgetmanager-input-dependencies"><html:msg key="gadgetmanager-prop-dependencies"></label></td>\
+								<td><input type="text" id="mw-gadgetmanager-input-dependencies" /></td>\
+							</tr>\
+							<tr>\
+								<td><label for="mw-gadgetmanager-input-messages"><html:msg key="gadgetmanager-prop-messages"></label></td>\
+								<td><input type="text" id="mw-gadgetmanager-input-messages" /></td>\
+							</tr>\
+						</table>\
+					</fieldset>\
+					<fieldset>\
+						<legend>Gadget settings</legend>\
+						<table>\
+							<tr>\
+								<td><label for="mw-gadgetmanager-input-category"><html:msg key="gadgetmanager-prop-category"></label></td>\
+								<td><select id="mw-gadgetmanager-input-category"></select></td>\
+							</tr>\
+							<tr>\
+								<td><label for="mw-gadgetmanager-input-rights"><html:msg key="gadgetmanager-prop-rights"></label></td>\
+								<td><input type="text" id="mw-gadgetmanager-input-rights" /></td>\
+							</tr>\
+							<tr>\
+								<td><label for="mw-gadgetmanager-input-default"><html:msg key="gadgetmanager-prop-default"></label></td>\
+								<td><input type="checkbox" id="mw-gadgetmanager-input-default" /></td>\
+							</tr>\
+							<tr>\
+								<td><label for="mw-gadgetmanager-input-hidden"><html:msg key="gadgetmanager-prop-hidden"></label></td>\
+								<td><input type="checkbox" id="mw-gadgetmanager-input-hidden" /></td>\
+							</tr>\
+							' + ( gm.conf.enableSharing ? '<tr>\
+								<td><label for="mw-gadgetmanager-input-shared"><html:msg key="gadgetmanager-prop-shared"></label></td>\
+								<td><input type="checkbox" id="mw-gadgetmanager-input-shared" /></td>\
+							</tr>\
+						' : '' ) + '</table>\
+					</fieldset>\
+				</form>'
+		},
+		/**
+		 * @var {Object} Static cache for suggestions by script prefix.
+		 */
+		 suggestCacheScripts = {},
+		/**
+		 * @var {Object} Static cache for suggestions by style prefix.
+		 */
+		 suggestCacheStyles = {},
+		/**
+		 * @var {Object} Static cache for suggestions by messages prefix.
+		 */
+		 suggestCacheMsgs = {},
+		/**
+		 * @var {Object} Complete static cache for module names. Lazy loaded from null.
+		 */
+		 suggestCacheDependencies = null,
+		/**
+		 * @var {Object} Complete static cache for all rights.
+		 */
+		 suggestCacheRights = gm.conf.allRights,
+		/**
+		 * @var {Number} Maximum number of autocomplete suggestions in the gadget editor input fields.
+		 */
+		 suggestLimit = 7,
+		 /**
+		  * @var {Array} List of category objects with their name, localized title and member count.
+		  */
+		 gadgetCategoriesCache = [];
+
+	/* Local functions */
+
+	/**
+	 * Remove all occurences of a value from an array.
+	 *
+	 * @param arr {Array} Array to be changed
+	 * @param val {Mixed} Value to be removed from the array
+	 * @return {Array} May or may not be changed, reference kept
+	 */
+	function arrayRemove( arr, val ) {
+		var i;
+		// Parentheses are crucial here. Without them, var i will be a
+		// boolean instead of a number, resulting in an infinite loop!
+		while ( ( i = arr.indexOf( val ) ) !== -1 ) {
+			arr.splice( i, 1 );
+		}
+		return arr;
+	}
+
+	/* Public functions */
+
+	gm.ui = {
+		/**
+		 * Initilizes the the page. For now just binding click handlers
+		 * to the anchor tags in the table.
+		 */
+		initUI: function() {
+			// Bind trigger to the links
+			$( '.mw-gadgetmanager-gadgets .mw-gadgetmanager-gadgets-title a' )
+				.click( function( e ) {
+					e.preventDefault();
+					var $el = $( this );
+					var gadget = {
+						id: $el.data( 'gadgetname' ),
+						displayTitle: $el.text(),
+						metadata: null
+					};
+					gm.ui.startGadgetEditor( gadget );
+				});
+		},
+
+		/**
+		 * Initialize the gadget editor dialog.
+		 *
+		 * @asynchronous
+		 * @param id {String}
+		 * @param displayTitle {String}
+		 */
+		startGadgetEditor: function( gadget ) {
+			// We need two things. Gadget meta-data and category information.
+			var done = 0, ready = 2;
+
+			gm.api.getGadgetMetadata( gadget.id, function( metadata, status ) {
+				// @todo Notification: If status is 'error'
+				gadget.metadata = metadata;
+				done++;
+				if ( done >= ready ) {
+					gm.ui.showFancyForm( gadget );
+				}
+			});
+
+			gm.api.getGadgetCategories( function( cats ) {
+				gadgetCategoriesCache = cats;
+				done++;
+				if ( done >= ready ) {
+					gm.ui.showFancyForm( gadget );
+				}
+			});
+		},
+
+		/**
+		 * Generate form, create a dialog and open it into view.
+		 *
+		 * @param gadget {Object}
+		 * @return {jQuery} The (dialogged) form.
+		 */
+		showFancyForm: function( gadget ) {
+			var $form = gm.ui.getFancyForm( gadget.metadata );
+			var buttons = {};
+			buttons[mw.msg( 'gadgetmanager-editor-save' )] = function() {
+				gm.api.doModifyGadget( gadget, function( status, msg ) {
+					alert( "Save result: \n- status: " + status + "\n- msg: " + msg );
+					/* @todo Notification
+					addNotification( {
+						msg: msg,
+						type: status !== 'error' ? 'success' : status,
+						timedActionDelay: 5,
+						timedAction: function(){
+							// refresh page
+						}
+					});
+					*/
+				});
+			};
+			return $form
+				.dialog({
+					autoOpen: true,
+					width: 800,
+					modal: true,
+					draggable: false,
+					resizable: false,
+					title: mw.message( 'gadgetmanager-editor-title', gadget.displayTitle ).escaped(),
+					buttons: buttons,
+					open: function() {
+						// Dialog is ready for action.
+						// Push out any notifications if some were queued up already between
+						// getting the gadget data and the display of the form.
+						/* @todo Notification
+						if ( gm.ui.notifications.length ) {
+							for ( in ) {
+								slice(i,1)_remove;
+								gm.ui.addNotification( $form, n[i] );
+							}
+						}
+						*/
+					}
+				});
+		},
+
+		/**
+		 * Generate a <form> for the given module.
+		 * Also binds events for submission and autocompletion.
+		 *
+		 * @param metadata {Object} Object to read and write to, used when saving
+		 * the gadget metadata back through the API.
+		 * @return {jQuery} The form.
+		 */
+		 getFancyForm: function( metadata ) {
+			var	nsGadgetId = mw.config.get( 'wgNamespaceIds' ).gadget,
+				$form = $( tpl.fancyForm ).localize();
+
+			// Module properties: scripts
+			$form.find( '#mw-gadgetmanager-input-scripts' ).createPropCloud({
+				props: metadata.module.scripts,
+				autocompleteSource: function( data, response ) {
+					// Use cache if available
+					if ( data.term in suggestCacheScripts ) {
+						response( suggestCacheScripts[data.term] );
+						return;
+					}
+					$.getJSON( mw.util.wikiScript( 'api' ), {
+							format: 'json',
+							action: 'query',
+							list: 'gadgetpages',
+							gpnamespace: nsGadgetId,
+							gpextension: 'js',
+							gpprefix: data.term
+						}, function( json ) {
+							if ( json && json.query && json.query.gadgetpages ) {
+								var suggestions = $.map( json.query.gadgetpages, function( val, i ) {
+									return val.pagename;
+								});
+								suggestions = suggestions.splice( 0, suggestLimit );
+
+								// Update cache
+								suggestCacheScripts[data.term] = suggestions;
+
+								response( suggestions );
+							} else {
+								response( [] );
+							}
+						}
+					);
+				},
+				prefix: 'mw-gadgetmanager',
+				removeTooltip: mw.msg( 'gadgetmanager-editor-removeprop-tooltip' ),
+				onAdd: function( prop ) { metadata.module.scripts.push( prop ); },
+				onRemove: function( prop ) { arrayRemove( metadata.module.scripts, prop ); }
+			});
+
+			// Module properties: styles
+			$form.find( '#mw-gadgetmanager-input-styles' ).createPropCloud({
+				props: metadata.module.styles,
+				autocompleteSource: function( data, response ) {
+					// Use cache if available
+					if ( data.term in suggestCacheStyles ) {
+						response( suggestCacheStyles[data.term] );
+						return;
+					}
+					$.getJSON( mw.util.wikiScript( 'api' ), {
+							format: 'json',
+							action: 'query',
+							list: 'gadgetpages',
+							gpnamespace: nsGadgetId,
+							gpextension: 'css',
+							gpprefix: data.term
+						}, function( json ) {
+							if ( json && json.query && json.query.gadgetpages ) {
+								var suggestions = $.map( json.query.gadgetpages, function( val, i ) {
+									return val.pagename;
+								});
+								suggestions = suggestions.splice( 0, suggestLimit );
+
+								// Update cache
+								suggestCacheStyles[data.term] = suggestions;
+
+								response( suggestions );
+							} else {
+								response( [] );
+							}
+						}
+					);
+				},
+				prefix: 'mw-gadgetmanager',
+				removeTooltip: mw.msg( 'gadgetmanager-editor-removeprop-tooltip' ),
+				onAdd: function( prop ) { metadata.module.styles.push( prop ); },
+				onRemove: function( prop ) { arrayRemove( metadata.module.styles, prop ); }
+			});
+
+			// Module properties: dependencies
+			$form.find( '#mw-gadgetmanager-input-dependencies' ).createPropCloud({
+				props: metadata.module.dependencies,
+				autocompleteSource: function( data, response ) {
+					if ( suggestCacheDependencies === null ) {
+						suggestCacheDependencies = mw.loader.getModuleNames();
+					}
+					var output = $.ui.autocomplete.filter( suggestCacheDependencies, data.term );
+					response( output.slice( 0, suggestLimit ) );
+				},
+				prefix: 'mw-gadgetmanager',
+				removeTooltip: mw.msg( 'gadgetmanager-editor-removeprop-tooltip' ),
+				onAdd: function( prop ) { metadata.module.dependencies.push( prop ); },
+				onRemove: function( prop ) { arrayRemove( metadata.module.dependencies, prop ); }
+			});
+
+			// Module properties: messages
+			$form.find( '#mw-gadgetmanager-input-messages' ).createPropCloud({
+				props: metadata.module.messages,
+				autocompleteSource: function( data, response ) {
+					// Use cache if available
+					if ( data.term in suggestCacheMsgs ) {
+						response( suggestCacheMsgs[data.term] );
+						return;
+					}
+					$.getJSON( mw.util.wikiScript( 'api' ), {
+							format: 'json',
+							action: 'query',
+							meta: 'allmessages',
+							amprefix: data.term,
+							amnocontent: true,
+							amlang: mw.config.get( 'wgContentLanguage' )
+						}, function( json ) {
+							if ( json && json.query && json.query.allmessages ) {
+								var suggestions = $.map( json.query.allmessages, function( val, i ) {
+									return val.name;
+								});
+								suggestions = suggestions.splice( 0, suggestLimit );
+
+								// Update cache
+								suggestCacheMsgs[data.term] = suggestions;
+
+								response( suggestions );
+							} else {
+								response( [] );
+							}
+						}
+					);
+				},
+				prefix: 'mw-gadgetmanager',
+				removeTooltip: mw.msg( 'gadgetmanager-editor-removeprop-tooltip' ),
+				onAdd: function( prop ) { metadata.module.messages.push( prop ); },
+				onRemove: function( prop ) { arrayRemove( metadata.module.messages, prop ); }
+			});
+
+			// Gadget settings: category
+			$form.find( '#mw-gadgetmanager-input-category' ).append( function() {
+				var	current = metadata.settings.category,
+					opts = '',
+					i = 0,
+					cat;
+				for ( ; i < gadgetCategoriesCache.length; i++ ) {
+					cat = gadgetCategoriesCache[i];
+					opts += mw.html.element( 'option', {
+						value: cat.name,
+						selected: cat.name === current
+					}, cat.title );
+				}
+				return opts;
+			}).change( function() {
+				metadata.settings.category = $(this).val();
+			});
+
+			// Gadget settings: rights
+			$form.find( '#mw-gadgetmanager-input-rights' ).createPropCloud({
+				props: metadata.settings.rights,
+				autocompleteSource: function( data, response ) {
+					var output = $.ui.autocomplete.filter( suggestCacheRights, data.term );
+					response( output.slice( 0, suggestLimit ) );
+				},
+				prefix: 'mw-gadgetmanager',
+				removeTooltip: mw.msg( 'gadgetmanager-editor-removeprop-tooltip' ),
+				onAdd: function( prop ) { metadata.settings.rights.push( prop ); },
+				onRemove: function( prop ) { arrayRemove( metadata.settings.rights, prop ); }
+			});
+
+			// Gadget settings: Default
+			$form.find( '#mw-gadgetmanager-input-default' )
+				.prop( 'checked', metadata.settings['default'] )
+				.change( function() {
+					metadata.settings['default'] = this.checked;
+				});
+
+			// Gadget settings: Hidden
+			$form.find( '#mw-gadgetmanager-input-hidden' )
+				.prop( 'checked', metadata.settings.hidden )
+				.change( function() { metadata.settings.hidden = this.checked; });
+
+			// Gadget settings: Shared
+			$form.find( '#mw-gadgetmanager-input-shared' )
+				.prop( 'checked', metadata.settings.shared )
+				.change( function() { metadata.settings.shared = this.checked; });
+
+
+			return $form;
+		}
+	};
+
+	// Launch on document ready
+	$( document ).ready( gm.ui.initUI );
+
+})();

extensions/Gadgets/modules/jquery.createPropCloud.js
@@ -1,0 +1,107 @@
+/**
+ * jQuery PropCloud plugin
+ * @author Timo Tijhof, 2011
+ */
+( function() {
+
+	function newPropHtml( label, o ) {
+		return $( '<span class="' + o.prefix + 'prop"></span>' )
+			.append(
+				$( '<span class="' + o.prefix + 'prop-label"></span>' ).text( label )
+			)
+			.append(
+				$( '<span class="' + o.prefix + 'prop-delete"></span> ')
+					.attr( 'title', o.removeTooltip )
+					.click( function() {
+						$(this).parent().remove();
+						o.onRemove( label );
+					}
+				)
+			);
+	}
+
+	/**
+	 * Create prop cloud around an input field.
+	 *
+	 * @example This is the HTML structure being created:
+	 *
+	 * <div class="editor-propcloud">
+	 *     <div class="editor-propcontainer">
+	 *         <span editor="jquery-prop">
+	 *             <span class="editor-prop-label"> .. </span>
+	 *             <span class="editor-prop-delete" title="Remove this item"></span>
+	 *         </span>
+	 *         <span class="editor-prop">
+	 *             <span class="editor-prop-label"> .. </span>
+	 *             <span class="editor-prop-delete" title="Remove this item"></span>
+	 *         </span>
+	 *     </div>
+	 *     <input class="editor-propinput" />
+	 * </div>
+	 *
+	 * @context {jQuery}
+	 * @param o {Object} All optional
+	 *     - prefix {String} Class name prefix
+	 *     - props {Array} Array of properties to start with
+	 *     - autocompleteSource {Function|Array} Source of autocomplete suggestions (required)
+	 *       See also http://jqueryui.com/demos/autocomplete/#options (source)
+	 *     - onAdd {Function} Callback when an item is added
+	 *     - onRemove {Function} Callback when an item is deleted
+	 *     - removeTooltip {String} Tooltip for the remove-icon
+	 *
+	 * @return {jQuery} prop cloud (input field inside)
+	 */
+	$.fn.createPropCloud = function( o ) {
+		// Some defaults
+		o = $.extend({
+			prefix: 'editor',
+			props: [],
+			autocompleteSource: [],
+			onAdd: function( prop ) {},
+			onRemove: function( prop ) {},
+			removeTooltip: 'Remove this item'
+		}, o );
+		o.prefix = o.prefix + '-';
+
+		var	$el = this.eq(0),
+			$input = $el.addClass( o.prefix + 'propinput' ),
+			$cloud = $input.wrap( '<div class="' + o.prefix + 'propcloud"></div>' ).parent(),
+			$container = $( '<div class="' + o.prefix + 'propcontainer"></div>' );
+
+		// Append while container is still off the dom
+		// This is faster and prevents visible build up
+		for ( var i = 0, props = o.props, len = props.length; i < len; i++ ) {
+			$container.append( newPropHtml( '' + props[i], o ) );
+		}
+
+		$input.autocomplete( {
+			// The source is entirly up to you
+			source: o.autocompleteSource,
+
+			// A value is choosen
+			// (ie. by pressing return/tab, clicking on suggestion, etc.)
+			select: function( e, data ){
+				var val = data.item.value;
+
+				// Prevent duplicate values
+				if ( o.props.indexOf( val ) === -1 ) {
+					$container.append( newPropHtml( val, o ) );
+					o.onAdd( val );
+				}
+
+				// Clear input whether duplicate (and ignored),
+				// or unique (and added to the PropCloud by now)
+				$input.val( '' );
+
+				// Return false,
+				// otherwise jQuery UI calls .val( val ) again
+				return false;
+			}
+		});
+
+		$cloud.prepend( $container );
+
+		return $cloud;
+	};
+
+})();

extensions/Gadgets/GadgetHooks.php
@@ -289,6 +289,23 @@ class GadgetHooks {
 	}
 
 	/**
+	 * MakeGlobalVariablesScript hook handler
+	 * @param $vars Array: Key/value pars for mw.config.set on this page.
+	 * @param $out OutputPage
+	 */
+	public static function makeGlobalVariablesScript( &$vars, $out ) {
+		if ( $out->getTitle()->equals( SpecialPage::getTitleFor( 'GadgetManager' ) ) ) {
+			global $wgGadgetEnableSharing;
+
+			$vars['gadgetManagerConf'] = array(
+				'enableSharing' => $wgGadgetEnableSharing,
+				'allRights' => User::getAllRights(),
+			);
+		}
+		return true;
+	}
+
+	/**
 	 * UnitTestsList hook handler
 	 * @param $files Array: List of extension test files
 	 */

commit 26b9005525335487b4e77acacf1f882e218a5f21

RL2: Some typo fixes in the JS files introduced in r96309
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/RL2@96661 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/Gadgets/modules/ext.gadgets.gadgetmanager.api.js
@@ -20,8 +20,8 @@
 
 	/**
 	 * For most returns from api.* functions, a clone is made when data from
-	 * cached is used. This is to avoid situations where later modifications
-	 * (ie. by the ajax editor) on the object affecting the cache (because
+	 * cache is used. This is to avoid situations where later modifications
+	 * (e.g. by the AJAX editor) to the object affect the cache (because
 	 * the object would otherwise be passed by reference).
 	 */
 	function objClone( obj ) {
@@ -34,10 +34,10 @@
 		 *     var a = { words: [ 'foo', 'bar','baz' ] };
 		 *     var b = $.extend( {}, a );
 		 *     b.words.push( 'quux' );
-		 *     a.words[4]; // quux !
+		 *     a.words[3]; // quux !
 		 * </code>
 		 */
-		 return $.extend( true /* resursive */, {}, obj );
+		 return $.extend( true /* recursive */, {}, obj );
 	}
 	function arrClone( arr ) {
 		return arr.slice();
@@ -151,7 +151,7 @@
 			 * - blob {Object} Gadget meta data
 			 * @param callback {Function} Called with two arguments:
 			 * - status ('ok' or 'error')
-			 * - msg (localized, something like "Successfull", "Conflict occurred" etc.)
+			 * - msg (localized, something like "Successful", "Conflict occurred" etc.)
 			 * @return {jqXHR|Null}: Null if served from cache, otherwise the jqXHR.
 			 */
 			doModifyGadget: function( gadget, callback ) {

extensions/Gadgets/modules/ext.gadgets.gadgetmanager.ui.js
@@ -117,7 +117,7 @@
 
 	gm.ui = {
 		/**
-		 * Initilizes the the page. For now just binding click handlers
+		 * Initializes the the page. For now just binding click handlers
 		 * to the anchor tags in the table.
 		 */
 		initUI: function() {

extensions/Gadgets/modules/jquery.createPropCloud.js
@@ -68,18 +68,18 @@
 			$cloud = $input.wrap( '<div class="' + o.prefix + 'propcloud"></div>' ).parent(),
 			$container = $( '<div class="' + o.prefix + 'propcontainer"></div>' );
 
-		// Append while container is still off the dom
-		// This is faster and prevents visible build up
+		// Append while container is still off the DOM
+		// This is faster and prevents visible build-up
 		for ( var i = 0, props = o.props, len = props.length; i < len; i++ ) {
 			$container.append( newPropHtml( '' + props[i], o ) );
 		}
 
 		$input.autocomplete( {
-			// The source is entirly up to you
+			// The source is entirely up to you
 			source: o.autocompleteSource,
 
 			// A value is choosen
-			// (ie. by pressing return/tab, clicking on suggestion, etc.)
+			// (e.g. by pressing return/tab, clicking on suggestion, etc.)
 			select: function( e, data ){
 				var val = data.item.value;
 