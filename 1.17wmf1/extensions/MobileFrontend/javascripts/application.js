df14aab98379ced8e331dec34acd27abcd64566b
preilly <preilly@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-19 21:49:08 +0000

    remove search based functions
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf@97563 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- 1.17wmf1/extensions/MobileFrontend/javascripts/application.js
+++ 1.17wmf1/extensions/MobileFrontend/javascripts/application.js
@@ -1,32 +1,3 @@
-var search = document.getElementById( 'search' );
-var clearSearch = document.getElementById( 'clearsearch' );
-
-initClearSearchLink();
-
-function initClearSearchLink() {
-    clearSearch.setAttribute( 'title','Clear' );
-    clearSearch.addEventListener( 'mousedown', clearSearchBox, true );
-    search.addEventListener( 'keyup', _handleClearSearchLink, false );
-}
-
-function _handleClearSearchLink() {
-    if ( clearSearch ) {
-    	if ( search.value.length > 0 ) {
-        	clearSearch.style.display = 'block';
-        } else {
-        	clearSearch.style.display = 'none';
-        }
-	}
-}
-
-function clearSearchBox( event ) {
-    search.value = '';
-    clearSearch.style.display = 'none';
-	if ( event ) {
-		event.preventDefault();
-	}
-}
-
 // I don't think this makes sense. Loading this here means that this button 
 // won't function until the page is loaded. It would probably be an 
 // improvement to just attach an onclick straight into the html.
