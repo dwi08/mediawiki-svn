dwi08/mediawiki-svn

commit 3c2efd9f5937968362e837cc26dbd7ddd2eb0eca

One less $wgTitle, and use dbw->update() instead of raw sql
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@77778 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

phase3/maintenance/rebuildrecentchanges.php
@@ -31,8 +31,6 @@ class RebuildRecentchanges extends Maintenance {
 	}
 
 	public function execute() {
-		global $wgTitle;
-		$wgTitle = Title::newFromText( "Rebuild recent changes script" );
 		$this->rebuildRecentChangesTablePass1();
 		$this->rebuildRecentChangesTablePass2();
 		$this->rebuildRecentChangesTablePass3();
@@ -137,10 +135,16 @@ class RebuildRecentchanges extends Maintenance {
 				$size = $dbw->selectField( 'revision', 'rev_len', array( 'rev_id' => $obj->rc_this_oldid ) );
 				$size = !is_null( $size ) ? intval( $size ) : 'NULL';
 
-				$sql3 = "UPDATE $recentchanges SET rc_last_oldid=$lastOldId,rc_new=$new,rc_type=$new," .
-					"rc_old_len=$lastSize,rc_new_len=$size " .
-					"WHERE rc_cur_id={$lastCurId} AND rc_this_oldid={$obj->rc_this_oldid}";
-				$dbw->query( $sql3 );
+				$dbw->update( 'recentchanges',
+					array(
+						'rc_old_len' => $lastSize,
+						'rc_new_len' => $size,
+					), array(
+						'rc_cur_id' => $lastCurId,
+						'rc_this_oldid' => $obj->rc_this_oldid,
+					),
+					__METHOD__
+				);
 
 				$lastOldId = intval( $obj->rc_this_oldid );
 				$lastSize = $size;

commit 04a2906ea7758694be25af0dce65f4a2cd0396d3

MFT r97544: fix bug 31007 regression in r77778
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/REL1_18@97545 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

phase3/maintenance/rebuildrecentchanges.php
@@ -132,7 +132,6 @@ class RebuildRecentchanges extends Maintenance {
 			} else {
 				# Grab the entry's text size
 				$size = $dbw->selectField( 'revision', 'rev_len', array( 'rev_id' => $obj->rc_this_oldid ) );
-				$size = !is_null( $size ) ? intval( $size ) : 'NULL';
 
 				$dbw->update( 'recentchanges',
 					array(