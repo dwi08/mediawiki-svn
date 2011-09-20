15b448cdf679c1096a9a229b81d06c6dba7ce214
preilly <preilly@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-20 23:40:58 +0000

    mft r97682
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf@97684 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- 1.18wmf1/extensions/MobileFrontend/views/information/disable.html.php
+++ 1.18wmf1/extensions/MobileFrontend/views/information/disable.html.php
@@ -2,22 +2,23 @@
 
 $lang = self::$code;
 $currentURL = self::$currentURL;
+$currentURL = str_replace('&mobileaction=disable_mobile_site', '', $currentURL);
 
 $disableHtml = <<<EOT
- <h1> 
-          {$areYouSure}
-        </h1> 
-        <p> 
-          {$explainDisable}
-        </p> 
-        <div id='disableButtons'> 
-          <form action='http://{$lang}.wikipedia.org/w/mobileRedirect.php' method='get'> 
-            <input name='to' type='hidden' value='{$currentURL}' /> 
-            <input name='expires_in_days' type='hidden' value='3650' /> 
-            <button id='disableButton' type='submit'>{$disableButton}</button> 
-          </form> 
-          <form action='/' method='get'> 
-            <button id='backButton' type='submit'>{$backButton}</button> 
-          </form> 
-        </div>
+ <h1>
+		  {$areYouSure}
+		</h1>
+		<p>
+		  {$explainDisable}
+		</p>
+		<div id='disableButtons'>
+		<form action='http://{$lang}.wikipedia.org/w/mobileRedirect.php' method='get'>
+			<input name='to' type='hidden' value='{$currentURL}' />
+			<input name='expires_in_days' type='hidden' value='3650' />
+			<button id='disableButton' type='submit'>{$disableButton}</button>
+		</form>
+		<form action='/' method='get'>
+			<button id='backButton' type='submit'>{$backButton}</button>
+		</form>
+		</div>
 EOT;
