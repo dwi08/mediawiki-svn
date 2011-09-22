dwi08/mediawiki-svn

commit 262375d3358f33c5eb7dd370e75ff1c88f044fd2

Refactor the factory/i18n/list/etc static methods from SpecialPage into their own class; there's no reason we need to be parsing them in every single SpecialPage subclass.  Leave all the methods as stubs in SpecialPage.php; if we required PHP 5.3 they could be replaced by a a __callStatic() magic method, but that doesn't work on PHP 5.2.
Also make a few changes to the functions available.  SpecialPageFactory::resolveAlias() now takes an optional subpage and returns array(<name>,<subpage>).  Similarly merge getPage() and getPageByAlias().  There were many examples of (extensions particularly) making dubious assumptions about the presence or absence of subpages or canonical-ness.

I didn't deprecate SpecialPage::getTitleFor() as it's got over six hundred calls.  I'm rather undecided on the best position of getPage()/executePath().  Although the latter needs cleanup anyway.

git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@86255 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/Title.php
@@ -1812,7 +1812,7 @@ class Title {
 			# If it's a special page, ditch the subpage bit and check again
 			if ( $this->getNamespace() == NS_SPECIAL ) {
 				$name = $this->getDBkey();
-				list( $name, /* $subpage */ ) = SpecialPage::resolveAliasWithSubpage( $name );
+				list( $name, /* $subpage */ ) = SpecialPageFactory::resolveAlias( $name );
 				if ( $name === false ) {
 					# Invalid special page, but we show standard login required message
 					return false;
@@ -3811,7 +3811,7 @@ class Title {
 				return (bool)wfFindFile( $this );
 			case NS_SPECIAL:
 				// valid special page
-				return SpecialPage::exists( $this->getDBkey() );
+				return SpecialPageFactory::exists( $this->getDBkey() );
 			case NS_MAIN:
 				// selflink, possibly with fragment
 				return $this->mDbkeyform == '';
@@ -4038,7 +4038,7 @@ class Title {
 	 */
 	public function isSpecial( $name ) {
 		if ( $this->getNamespace() == NS_SPECIAL ) {
-			list( $thisName, /* $subpage */ ) = SpecialPage::resolveAliasWithSubpage( $this->getDBkey() );
+			list( $thisName, /* $subpage */ ) = SpecialPageFactory::resolveAlias( $this->getDBkey() );
 			if ( $name == $thisName ) {
 				return true;
 			}
@@ -4054,9 +4054,9 @@ class Title {
 	 */
 	public function fixSpecialName() {
 		if ( $this->getNamespace() == NS_SPECIAL ) {
-			$canonicalName = SpecialPage::resolveAlias( $this->mDbkeyform );
+			list( $canonicalName, /*...*/ ) = SpecialPageFactory::resolveAlias( $this->mDbkeyform );
 			if ( $canonicalName ) {
-				$localName = SpecialPage::getLocalNameFor( $canonicalName );
+				$localName = SpecialPageFactory::getLocalNameFor( $canonicalName );
 				if ( $localName != $this->mDbkeyform ) {
 					return Title::makeTitle( NS_SPECIAL, $localName );
 				}

includes/AutoLoader.php
@@ -226,6 +226,7 @@ $wgAutoloadLocalClasses = array(
 	'SpecialMypage' => 'includes/SpecialPage.php',
 	'SpecialMytalk' => 'includes/SpecialPage.php',
 	'SpecialPage' => 'includes/SpecialPage.php',
+	'SpecialPageFactory' => 'includes/SpecialPageFactory.php',
 	'SpecialRedirectToSpecial' => 'includes/SpecialPage.php',
 	'SquidUpdate' => 'includes/SquidUpdate.php',
 	'SquidPurgeClient' => 'includes/SquidPurgeClient.php',

includes/ChangesList.php
@@ -558,7 +558,7 @@ class OldChangesList extends ChangesList {
 			$this->insertLog( $s, $logtitle, $rc->mAttribs['rc_log_type'] );
 		// Log entries (old format) or log targets, and special pages
 		} elseif( $rc->mAttribs['rc_namespace'] == NS_SPECIAL ) {
-			list( $name, $subpage ) = SpecialPage::resolveAliasWithSubpage( $rc->mAttribs['rc_title'] );
+			list( $name, $subpage ) = SpecialPageFactory::resolveAlias( $rc->mAttribs['rc_title'] );
 			if( $name == 'Log' ) {
 				$this->insertLog( $s, $rc->getTitle(), $subpage );
 			}
@@ -694,7 +694,7 @@ class EnhancedChangesList extends ChangesList {
 			$watched = false;
 		// Log entries (old format) and special pages
 		} elseif( $rc->mAttribs['rc_namespace'] == NS_SPECIAL ) {
-			list( $specialName, $logtype ) = SpecialPage::resolveAliasWithSubpage( $rc->mAttribs['rc_title'] );
+			list( $specialName, $logtype ) = SpecialPageFactory::resolveAlias( $rc->mAttribs['rc_title'] );
 			if ( $specialName == 'Log' ) {
 				# Log updates, etc
 				$logname = LogPage::logName( $logtype );

includes/Linker.php
@@ -376,7 +376,7 @@ class Linker {
 
 	static function normaliseSpecialPage( Title $title ) {
 		if ( $title->getNamespace() == NS_SPECIAL ) {
-			list( $name, $subpage ) = SpecialPage::resolveAliasWithSubpage( $title->getDBkey() );
+			list( $name, $subpage ) = SpecialPageFactory::resolveAlias( $title->getDBkey() );
 			if ( !$name ) {
 				return $title;
 			}

includes/OutputPage.php
@@ -2652,8 +2652,7 @@ class OutputPage {
 		$ns = $title->getNamespace();
 		$nsname = MWNamespace::exists( $ns ) ? MWNamespace::getCanonicalName( $ns ) : $title->getNsText();
 		if ( $ns == NS_SPECIAL ) {
-			$parts = SpecialPage::resolveAliasWithSubpage( $title->getDBkey() );
-			$canonicalName = $parts[0];
+			list( $canonicalName, /*...*/ ) = SpecialPageFactory::resolveAlias( $title->getDBkey() );
 		} else {
 			$canonicalName = false; # bug 21115
 		}

includes/PrefixSearch.php
@@ -85,15 +85,13 @@ class PrefixSearch {
 
 		// Unlike SpecialPage itself, we want the canonical forms of both
 		// canonical and alias title forms...
-		SpecialPage::initList();
-		SpecialPage::initAliasList();
 		$keys = array();
-		foreach( array_keys( SpecialPage::$mList ) as $page ) {
+		foreach( SpecialPageFactory::getList() as $page => $class ) {
 			$keys[$wgContLang->caseFold( $page )] = $page;
 		}
 
 		foreach( $wgContLang->getSpecialPageAliases() as $page => $aliases ) {
-			if( !array_key_exists( $page, SpecialPage::$mList ) ) {# bug 20885
+			if( !array_key_exists( $page, SpecialPageFactory::getList() ) ) {# bug 20885
 				continue;
 			}
 

includes/Skin.php
@@ -303,6 +303,7 @@ abstract class Skin {
 	 * Special:Contributions mark the user which they are relevant to so that
 	 * things like the toolbox can display the information they usually are only
 	 * able to display on a user's userpage and talkpage.
+	 * @return User
 	 */
 	public function getRelevantUser() {
 		if ( isset($this->mRelevantUser) ) {
@@ -485,7 +486,7 @@ abstract class Skin {
 		if ( $title->getNamespace() == NS_SPECIAL ) {
 			$type = 'ns-special';
 			// bug 23315: provide a class based on the canonical special page name without subpages
-			list( $canonicalName ) = SpecialPage::resolveAliasWithSubpage( $title->getDBkey() );
+			list( $canonicalName ) = SpecialPageFactory::resolveAlias( $title->getDBkey() );
 			if ( $canonicalName ) {
 				$type .= ' ' . Sanitizer::escapeClass( "mw-special-$canonicalName" );
 			} else {
@@ -1128,7 +1129,7 @@ abstract class Skin {
 	}
 
 	static function makeSpecialUrl( $name, $urlaction = '' ) {
-		$title = SpecialPage::getTitleFor( $name );
+		$title = SpecialPage::getSafeTitleFor( $name );
 		return $title->getLocalURL( $urlaction );
 	}
 

includes/SkinLegacy.php
@@ -432,7 +432,7 @@ class LegacyTemplate extends BaseTemplate {
 	function specialPagesList() {
 		global $wgContLang, $wgServer, $wgRedirectScript;
 
-		$pages = SpecialPage::getUsablePages();
+		$pages = SpecialPageFactory::getUsablePages();
 
 		foreach ( $pages as $name => $page ) {
 			$pages[$name] = $page->getDescription();

commit b4ad5510a3c539229a1538757d0d4474e664a47a

MFT r97857 to 1.18wmf1 -- regression from r86255 (bug 31100)
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf@97859 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

1.18wmf1/includes/Title.php
@@ -4114,9 +4114,9 @@ class Title {
 	 */
 	public function fixSpecialName() {
 		if ( $this->getNamespace() == NS_SPECIAL ) {
-			list( $canonicalName, /*...*/ ) = SpecialPageFactory::resolveAlias( $this->mDbkeyform );
+			list( $canonicalName, $par ) = SpecialPageFactory::resolveAlias( $this->mDbkeyform );
 			if ( $canonicalName ) {
-				$localName = SpecialPageFactory::getLocalNameFor( $canonicalName );
+				$localName = SpecialPageFactory::getLocalNameFor( $canonicalName, $par );
 				if ( $localName != $this->mDbkeyform ) {
 					return Title::makeTitle( NS_SPECIAL, $localName );
 				}

1.18wmf1/tests/phpunit/includes/TitleTest.php
@@ -15,4 +15,26 @@ class TitleTest extends MediaWikiTestCase {
 		}
 	}
 
+	/**
+	 * @dataProvider dataBug31100
+	 */
+	function testBug31100FixSpecialName( $text, $expectedParam ) {
+		$title = Title::newFromText( $text );
+		$fixed = $title->fixSpecialName();
+		$stuff = explode( '/', $fixed->getDbKey(), 2 );
+		if ( count( $stuff ) == 2 ) {
+			$par = $stuff[1];
+		} else {
+			$par = null;
+		}
+		$this->assertEquals( $expectedParam, $par, "Bug 31100 regression check: Title->fixSpecialName() should preserve parameter" );
+	}
+
+	function dataBug31100() {
+		return array(
+			array( 'Special:Version', null ),
+			array( 'Special:Version/', '' ),
+			array( 'Special:Version/param', 'param' ),
+		);
+	}
 }