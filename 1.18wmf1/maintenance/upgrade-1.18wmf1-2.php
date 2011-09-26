1b63f0158af6346dba3afc40d9f3801270407c9c
asher <asher@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-26 21:35:40 +0000

    update lt schema upgrade script to work on secondary db's
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf@98178 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- 1.18wmf1/maintenance/upgrade-1.18wmf1-2.php
+++ 1.18wmf1/maintenance/upgrade-1.18wmf1-2.php
@@ -1,96 +1,115 @@
 <?php
 
-require( dirname( __FILE__ ) . '/commandLine.inc' );
+require_once( dirname( __FILE__ ) . '/Maintenance.php' );
 require( dirname( __FILE__ ) . '/../../wmf-config/InitialiseSettings.php' );
 
-doAllSchemaChanges();
+class SchemaMigration extends Maintenance {
 
-function doAllSchemaChanges() {
-	global $wgLBFactoryConf, $wgConf;
+    public function __construct() {
+        parent::__construct();
+        $this->mDescription = "Run Schema Migrations for branch against all wikis";
+        $this->addOption( 'secondary', 'Run on secondary / non-prod slaves', false, false );
+    }
 
-	$sectionLoads = $wgLBFactoryConf['sectionLoads'];
-	$sectionsByDB = $wgLBFactoryConf['sectionsByDB'];
-	$rootPass = trim( wfShellExec( '/home/wikipedia/bin/mysql_root_pass' ) );
+    function doAllSchemaChanges() {
+        global $wgLBFactoryConf, $wgConf;
 
-	// Compile wiki lists
-	$wikisBySection = array();
-	foreach ( $wgConf->getLocalDatabases() as $wiki ) {
-		if ( isset( $sectionsByDB[$wiki] ) ) {
-			$wikisBySection[$sectionsByDB[$wiki]][] = $wiki;
-		} else {
-			$wikisBySection['DEFAULT'][] = $wiki;
-		}
-	}
+        if ( $this->getOption( 'secondary' ) ) { 
+            require( dirname( __FILE__ ) . '/../../wmf-config/db-secondary.php' );
+        }
 
-	// Do the upgrades
-	foreach ( $sectionLoads as $section => $loads ) {
-		$master = true;
-		foreach ( $loads as $server => $load ) {
-			if ( $master ) {
-				echo "Skipping $section master $server\n";
-				$master = false;
-				continue;
-			}
+        $sectionLoads = $wgLBFactoryConf['sectionLoads'];
+        $sectionsByDB = $wgLBFactoryConf['sectionsByDB'];
 
-			$db = new DatabaseMysql(
-				$server,
-				'root',
-				$rootPass,
-				false, /* dbName */
-				0, /* flags, no transactions */
-				'' /* prefix */
-			);
+        $rootPass = trim( wfShellExec( '/home/wikipedia/bin/mysql_root_pass' ) );
 
-			foreach ( $wikisBySection[$section] as $wiki ) {
-				$db->selectDB( $wiki );
-				upgradeWiki( $db );
-				while ( $db->getLag() > 10 ) {
-					echo "Waiting for $server to catch up to master.\n";
-					sleep( 60 );
-				}
-			}
-		}
-	}
+        // Compile wiki lists
+        $wikisBySection = array();
+        foreach ( $wgConf->getLocalDatabases() as $wiki ) {
+            if ( isset( $sectionsByDB[$wiki] ) ) {
+                $wikisBySection[$sectionsByDB[$wiki]][] = $wiki;
+            } else {
+                $wikisBySection['DEFAULT'][] = $wiki;
+            }
+        }
 
-	echo "All done (except masters).\n";
+        // Do the upgrades
+        foreach ( $sectionLoads as $section => $loads ) {
+            $master = true;
+            foreach ( $loads as $server => $load ) {
+                if ( $master ) {
+                    echo "Skipping $section master $server\n";
+                    $master = false;
+                    continue;
+                }
+
+                $db = new DatabaseMysql(
+                    $server,
+                    'root',
+                    $rootPass,
+                    false, /* dbName */
+                    0, /* flags, no transactions */
+                    '' /* prefix */
+                );
+
+                foreach ( $wikisBySection[$section] as $wiki ) {
+                    $db->selectDB( $wiki );
+                    $this->upgradeWiki( $db );
+                    if ( !$this->getOption( 'secondary' ) ) { 
+                        while ( $db->getLag() > 10 ) {
+                            echo "Waiting for $server to catch up to master.\n";
+                            sleep( 60 );
+                        }
+                    }
+                }
+            }
+        }
+
+        echo "All done (except masters).\n";
+    }
+
+    function upgradeWiki( $db ) {
+        global $wgConf;
+        $wiki = $db->getDBname();
+        $server = $db->getServer();
+
+        $upgradeLogRow = $db->selectRow( 'updatelog',
+            'ul_key',
+            array( 'ul_key' => '1.18wmf1-lt' ),
+            __FUNCTION__ );
+        if ( $upgradeLogRow ) {
+            echo $db->getDBname() . ": already done\n";
+            return;
+        }
+
+        echo "$server $wiki 1.18wmf1-lt";
+
+        if ( $wgConf->get( 'wmgUseLiquidThreads', $wiki ) && $wiki != "test2wiki") {
+            echo " liquidthreads";
+            $this->sourceUpgradeFile( $db, dirname( __FILE__ ) .'/ums_conversation.sql' );
+        } else { 
+            echo " no-op";
+        }
+
+        $db->insert( 'updatelog', 
+            array( 'ul_key' => '1.18wmf1-lt' ),
+            __FUNCTION__ );
+        echo " ok\n";
+    }
+
+    function sourceUpgradeFile( $db, $file ) {
+        if ( !file_exists( $file ) ) {
+            echo "File missing: $file\n";
+            exit( 1 );
+        }
+        $db->sourceFile( $file );
+    }
+    
+    function execute() { 
+        $this->doAllSchemaChanges();
+    }
 }
 
-function upgradeWiki( $db ) {
-	global $wgConf;
-	$wiki = $db->getDBname();
-	$server = $db->getServer();
-
-	$upgradeLogRow = $db->selectRow( 'updatelog',
-		'ul_key',
-		array( 'ul_key' => '1.18wmf1-lt' ),
-		__FUNCTION__ );
-	if ( $upgradeLogRow ) {
-		echo $db->getDBname() . ": already done\n";
-		return;
-    } 
-
-	echo "$server $wiki 1.18wmf1-lt";
-	
-	if ( $wgConf->get( 'wmgUseLiquidThreads', $wiki ) && $wiki != "test2wiki") {
-		echo " liquidthreads";
-		sourceUpgradeFile( $db, dirname( __FILE__ ) .'/ums_conversation.sql' );
-	} else {
-		echo " no-op";
-	}
-	
-	$db->insert( 'updatelog', 
-		array( 'ul_key' => '1.18wmf1-lt' ),
-		__FUNCTION__ );
-    echo " ok\n"; 
-
-}
-
-function sourceUpgradeFile( $db, $file ) {
-	if ( !file_exists( $file ) ) {
-		echo "File missing: $file\n";
-		exit( 1 );
-	}
-	$db->sourceFile( $file );
-}
-
+$maintClass = "SchemaMigration";
+require_once( RUN_MAINTENANCE_IF_MAIN );
 
