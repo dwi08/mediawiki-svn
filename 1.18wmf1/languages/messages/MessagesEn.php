a1ab410cfb29f1283c110dcab0e3fee9c02f9a43
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-29 20:32:14 +0000

    1.18wmf1 MFT r93431, r98076, r98185
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf@98444 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- 1.18wmf1/languages/messages/MessagesEn.php
+++ 1.18wmf1/languages/messages/MessagesEn.php
@@ -1177,6 +1177,8 @@ You may have already successfully changed your password or requested a new tempo
 'passwordreset-pretext'        => '{{PLURAL:$1||Enter one of the pieces of data below}}',
 'passwordreset-username'       => 'Username:',
 'passwordreset-domain'         => 'Domain:',
+'passwordreset-capture'        => 'View the resulting e-mail?',
+'passwordreset-capture-help'   => 'If you check this box, the e-mail (with the temporary password) will be shown to you as well as being sent to the user.',
 'passwordreset-email'          => 'E-mail address:',
 'passwordreset-emailtitle'     => 'Account details on {{SITENAME}}',
 'passwordreset-emailtext-ip'   => 'Someone (probably you, from IP address $1) requested a reminder of your
@@ -1968,6 +1970,7 @@ Your e-mail address is not revealed when other users contact you.',
 'right-siteadmin'             => 'Lock and unlock the database',
 'right-override-export-depth' => 'Export pages including linked pages up to a depth of 5',
 'right-sendemail'             => 'Send e-mail to other users',
+'right-passwordreset'         => 'View password reset e-mails',
 
 # User rights log
 'rightslog'                  => 'User rights log',
