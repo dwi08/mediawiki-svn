76aaab1bf23a517405abdfcca2dcec356cdfaf70
awjrichards <awjrichards@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-30 19:15:47 +0000

    Tag branches/fundraising/vendors/mediawiki/current as branches/fundraising/vendors/mediawiki/1.17wmf1_r98554.
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/fundraising@98556 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ vendors/mediawiki/1.17wmf1_r98554/languages/messages/MessagesKs.php
@@ -0,0 +1,13 @@
+<?php
+/** Kashmiri (कश्मीरी - (كشميري))
+ *
+ * See MessagesQqq.php for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ */
+
+$fallback = 'ks-arab';
+
