550781e71088fb8987115c845581a144c64a96d4
awjrichards <awjrichards@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-30 19:04:39 +0000

    Revert r98542, i did not set up this vendor branch correctly to begin with, starting over.
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/fundraising@98554 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- vendors/mediawiki/1.17wmf1/extensions/FlaggedRevs/maintenance/wikimedia-periodic-update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for db in `</home/wikipedia/common/flaggedrevs.dblist`;do
-	echo $db
-	php -n /home/wikipedia/common/multiversion/MWScript.php extensions/FlaggedRevs/maintenance/updateStats.php $db
-done
--- vendors/mediawiki/1.17wmf1/extensions/FlaggedRevs/mysql/patch-fi_img_timestamp-without-update.sql
+++
@@ -1,3 +0,0 @@
--- Fix bad fi_img_timestamp definition
-ALTER TABLE /*$wgDBprefix*/flaggedimages
-    CHANGE fi_img_timestamp fi_img_timestamp varbinary(14) NULL;
