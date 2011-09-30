163457b9c992b9a14049896666729c6d202df415
preilly <preilly@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-09-30 21:21:46 +0000

    mft r98566
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf@98568 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- 1.18wmf1/extensions/MobileFrontend/views/layout/application.html.php
+++ 1.18wmf1/extensions/MobileFrontend/views/layout/application.html.php
@@ -4,7 +4,7 @@ global $wgExtensionAssetsPath, $wgAppleTouchIcon;
 $dir = self::$dir;
 $code = self::$code;
 
-if( $wgAppleTouchIcon !== false ) {
+if ( $wgAppleTouchIcon !== false ) {
 	$appleTouchIconTag = Html::element( 'link', array( 'rel' => 'apple-touch-icon', 'href' => $wgAppleTouchIcon ) );
 } else {
 	$appleTouchIconTag = "";
@@ -20,20 +20,20 @@ $applicationHtml = <<<EOT
   "http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd">
 <html lang='{$code}' dir='{$dir}' xml:lang='{$code}' xmlns='http://www.w3.org/1999/xhtml'>
   <head>
-    <title>{$htmlTitle}</title>
-    <meta http-equiv="content-type" content="application/xhtml+xml; charset=utf-8" />
-    <link href='{$wgExtensionAssetsPath}/MobileFrontend/stylesheets/{$cssFileName}.css' media='all' rel='Stylesheet' type='text/css' />
-    <meta name="ROBOTS" content="NOINDEX, NOFOLLOW" />
-    <meta name = "viewport" content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
-    ${appleTouchIconTag}
-    <script type='text/javascript'>
-      //<![CDATA[
-        var title = "{$htmlTitle}";
-        function shouldCache() {
-          return true;
-        }
-      //]]>
-    </script>
+	<title>{$htmlTitle}</title>
+	<meta http-equiv="content-type" content="application/xhtml+xml; charset=utf-8" />
+	<link href='{$wgExtensionAssetsPath}/MobileFrontend/stylesheets/{$cssFileName}.css' media='all' rel='Stylesheet' type='text/css' />
+	<meta name="ROBOTS" content="NOINDEX, NOFOLLOW" />
+	<meta name = "viewport" content = "width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+	${appleTouchIconTag}
+	<script type='text/javascript'>
+	  //<![CDATA[
+		var title = "{$htmlTitle}";
+		function shouldCache() {
+		  return true;
+		}
+	  //]]>
+	</script>
   </head>
   <body>
 	{$searchWebkitHtml}
@@ -42,7 +42,7 @@ $applicationHtml = <<<EOT
 	{$contentHtml}
 	</div>
 	{$footerHtml}
-	 <script type="text/javascript" language="javascript" src="{$wgExtensionAssetsPath}/MobileFrontend/javascripts/application.js?version=20110919T172820Z"></script>
+	 <script type="text/javascript" language="javascript" src="{$wgExtensionAssetsPath}/MobileFrontend/javascripts/application.js?version=20110920T172820Z"></script>
   </body>
 </html>
 EOT;
