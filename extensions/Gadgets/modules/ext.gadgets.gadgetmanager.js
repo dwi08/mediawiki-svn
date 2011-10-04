dwi08/mediawiki-svn

commit 471be2ed15a2db78b45434b351ab2cbbc5a78404

[RL2] Entire gadget row is now clickable
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/RL2@98735 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/Gadgets/modules/ext.gadgets.gadgetmanager.js
@@ -142,6 +142,26 @@
 				}
 			} );
 
+			// Entire gadget list item is clickable
+			$( '.mw-gadgets-gadget' ).click( function( e ) {
+				e.preventDefault();
+				var	t,
+					id = $( this ).data( 'gadget-id' );
+
+				if ( ga.conf.userIsAllowed['gadgets-definition-edit'] ) {
+					ga.ui.startGadgetEditor( id );
+					return;
+				}
+				// Use localized page name if possible to avoid redirect
+				if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Gadgets' ) {
+					t = new mw.Title( mw.config.get( 'wgTitle' ) + '/' + id, -1 );
+				} else {
+					t = new mw.Title( 'Gadgets/' + id, -1 );
+				}
+				window.location.href = t.getUrl();
+				return false;
+			} );
+
 			if ( ga.conf.userIsAllowed['gadgets-definition-create'] ) {
 				var createTab = mw.util.addPortletLink(
 					// Not all skins use the new separated tabs yet,

extensions/Gadgets/modules/ext.gadgets.gadgetmanager.css
@@ -23,6 +23,13 @@
 }
 
 /**
+ * Gadget list
+ */
+.client-js .mw-gadgets-gadget:hover {
+	cursor: pointer;
+}
+
+/**
  * The PropCloud
  */
 .mw-gadgetmanager-propcloud {

commit 81a890ea83dd89555f292dc802b7d34b386c4f8d

[RL2] Stop propagation of clicks on other anchor tags inside the gadget row. * Fixes bug introduced in r98735
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/RL2@98942 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/Gadgets/modules/ext.gadgets.gadgetmanager.js
@@ -185,7 +185,12 @@
 					t = new mw.Title( 'Gadgets/' + id, -1 );
 				}
 				window.location.href = t.getUrl();
-			} );
+
+			} ).find( 'a' ).click( function( e ) {
+				// Avoid other links from becoming unclickable,
+				// Don't let clicks on those bubble up
+				e.stopPropagation();
+			});
 
 			if ( ga.conf.userIsAllowed['gadgets-definition-create'] ) {
 				var createTab = mw.util.addPortletLink(