bdff45c5aa27bbb0bbfcb1c4a11d786326afe5bf
aaron <aaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-13 05:38:44 +0000

    fixed warnings for stats update (for flagged protection)
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf@99669 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- 1.18wmf1/extensions/FlaggedRevs/dataclasses/FlaggedRevsStats.php
+++ 1.18wmf1/extensions/FlaggedRevs/dataclasses/FlaggedRevsStats.php
@@ -197,11 +197,19 @@ class FlaggedRevsStats {
 	}
 
 	private function getReviewTimesAnons( $dbCache ) {
+		$result = array(
+			'average'       => 0,
+			'median'        => 0,
+			'percTable'     => array(),
+			'sampleSize'    => 0,
+			'sampleStartTS' => null,
+			'sampleEndTS'   => null
+		);
+		if ( FlaggedRevs::useOnlyIfProtected() ) {
+			return $result; // disabled
+		}
 		$aveRT = $medianRT = 0;
 		$rPerTable = array(); // review wait percentiles
-		if ( FlaggedRevs::useOnlyIfProtected() ) {
-			return array( $aveRT, $medianRT, $rPerTable ); // disabled
-		}
 		$nowUnix = wfTimestamp( TS_UNIX ); // current time in UNIX TS
 		# Only go so far back...otherwise we will get garbage values due to 
 		# the fact that FlaggedRevs wasn't enabled until after a while.
@@ -351,15 +359,14 @@ class FlaggedRevsStats {
 				$rPerTable[$percentile] = $times[$rank];
 			}
 			#echo "(sampled ".count($times)." edits)...";
+			$result['average']       = $aveRT;
+			$result['median']        = $medianRT;
+			$result['percTable']     = $rPerTable;
+			$result['sampleSize']    = count( $times );
+			$result['sampleStartTS'] = $minTSUnix;
+			$result['sampleEndTS']   = $maxTSUnix;
 		}
 
-		return array(
-			'average'		=> $aveRT,
-			'median'		=> $medianRT,
-			'percTable'		=> $rPerTable,
-			'sampleSize'	=> count( $times ),
-			'sampleStartTS'	=> $minTSUnix,
-			'sampleEndTS'	=> $maxTSUnix
-		);
+		return $result;
 	}
 }
