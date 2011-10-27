2313f8ea856d8a27f67c0294728d08c70d0e059f
preilly <preilly@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-10-27 21:02:26 +0000

    mft r101052
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf@101053 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- 1.18wmf1/extensions/MobileFrontend/MobileFrontend.php
+++ 1.18wmf1/extensions/MobileFrontend/MobileFrontend.php
@@ -337,15 +337,13 @@ class ExtMobileFrontend {
 		
 		$languageUrls = array();
 		
-		if ( $wgLanguageCode == 'en' ) {
-			$languageUrls[] = array(
-				'href' => self::$currentURL,
-				'text' => self::$htmlTitle,
-				'language' => $wgContLang->getLanguageName( $wgLanguageCode ),
-				'class' => 'interwiki-' . $wgLanguageCode,
-				'lang' => $wgLanguageCode,
-			);
-		}
+		$languageUrls[] = array(
+			'href' => self::$currentURL,
+			'text' => self::$htmlTitle,
+			'language' => $wgContLang->getLanguageName( $wgLanguageCode ),
+			'class' => 'interwiki-' . $wgLanguageCode,
+			'lang' => $wgLanguageCode,
+		);
 		
 		foreach( $wgOut->getLanguageLinks() as $l ) {
 			if ( preg_match( '!^(\w[-\w]*\w):(.+)$!', $l, $m ) ) {
@@ -365,7 +363,7 @@ class ExtMobileFrontend {
 			$queryDelimiter = ( !empty( $parsedUrl['query'] ) ) ? '?' : '';
 			$languageUrl = $parsedUrl['scheme'] . '://' .  $parsedUrl['host'] . $parsedUrl['path'] . $queryDelimiter . $parsedUrl['query'] . $fragmentDelimiter . $parsedUrl['fragment'];
 
-			if ( substr( $languageUrl, 0, 1 ) == ':') {
+			if ( substr( $languageUrl, 0, 1 ) == ':' ) {
 				$languageUrl = substr( $languageUrl, 1 );
 			}
 
