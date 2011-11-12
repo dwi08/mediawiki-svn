335015cbe6e666d5f09422ae6d114846cebe170e
awjrichards <awjrichards@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-11-12 02:34:33 +0000

    Updating layout of form enabling script to reflect the layout of the spreadsheet tracking live forms; removing cruft
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/fundraising@102843 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- deployment/payments_1.17/DonationInterfaceFormSettings.php
+++ deployment/payments_1.17/DonationInterfaceFormSettings.php
@@ -6,57 +6,53 @@
  * All these exist, but are disabled by default. 
  * Uncomment to enable.
  */
-//$wgAutoloadClasses['Gateway_Form_OneStepTwoColumn'] = $donationinterface_dir . 'gateway_forms/OneStepTwoColumn.php';
-//$wgAutoloadClasses['Gateway_Form_TwoColumnPayPal'] = $donationinterface_dir . 'gateway_forms/TwoColumnPayPal.php';
-//$wgAutoloadClasses['Gateway_Form_TwoColumnLetter'] = $donationinterface_dir . 'gateway_forms/TwoColumnLetter.php';
-//$wgAutoloadClasses['Gateway_Form_TwoColumnLetter2'] = $donationinterface_dir . 'gateway_forms/TwoColumnLetter2.php';
-//$wgAutoloadClasses['Gateway_Form_TwoColumnLetter3'] = $donationinterface_dir . 'gateway_forms/TwoColumnLetter3.php';
-//$wgAutoloadClasses['Gateway_Form_TwoColumnLetter4'] = $donationinterface_dir . 'gateway_forms/TwoColumnLetter4.php';
-//$wgAutoloadClasses['Gateway_Form_TwoColumnLetter5'] = $donationinterface_dir . 'gateway_forms/TwoColumnLetter5.php';
-//$wgAutoloadClasses['Gateway_Form_TwoColumnLetter6'] = $donationinterface_dir . 'gateway_forms/TwoColumnLetter6.php';
-//$wgAutoloadClasses['Gateway_Form_TwoColumnLetter7'] = $donationinterface_dir . 'gateway_forms/TwoColumnLetter7.php';
-//$wgAutoloadClasses['Gateway_Form_TwoStepTwoColumnLetterCA'] = $donationinterface_dir . 'gateway_forms/TwoStepTwoColumnLetterCA.php';
-//$wgAutoloadClasses['Gateway_Form_TwoStepTwoColumnLetter2'] = $donationinterface_dir . 'gateway_forms/TwoStepTwoColumnLetter2.php';
-//$wgAutoloadClasses['Gateway_Form_TwoStepTwoColumnPremium'] = $donationinterface_dir . 'gateway_forms/TwoStepTwoColumnPremium.php';
-//$wgAutoloadClasses['Gateway_Form_TwoStepTwoColumnPremiumUS'] = $donationinterface_dir . 'gateway_forms/TwoStepTwoColumnPremiumUS.php';
-//$wgAutoloadClasses['Gateway_Form_SingleColumn'] = $donationinterface_dir . 'gateway_forms/SingleColumn.php';
 
-if ( !isset( $wgDonationDataAllowedHtmlForms )) $wgDonationDataAllowedHtmlForms = array();
 
 /**
  * DonationInterface RapidHTML whitelist additions
  * These will apply to all enabled adapters 
  */
-//example addition of an extension-wide form
-//$wgDonationInterfaceAllowedHtmlForms['demo'] = $wgDonationInterfaceHtmlFormDir .'/demo.html';
 
 /** Sync up with the gateways again, in such a way that we get all the clobal changes without ditching what we already have.  **/
+if ( !isset( $wgDonationDataAllowedHtmlForms )) $wgDonationDataAllowedHtmlForms = array();
 $wgGlobalCollectGatewayAllowedHtmlForms = array_merge( $wgGlobalCollectGatewayAllowedHtmlForms, $wgDonationDataAllowedHtmlForms );
 $wgPayflowProGatewayAllowedHtmlForms = array_merge( $wgPayflowProGatewayAllowedHtmlForms, $wgDonationDataAllowedHtmlForms );
 
 /**
  * GlobalCollect RapidHTML whitelist additions
  */
-$wgGlobalCollectGatewayAllowedHtmlForms['webitects_2_3step'] = $wgGlobalCollectGatewayHtmlFormDir . '/webitects_2_3step.html';
-$wgGlobalCollectGatewayAllowedHtmlForms['webitects_2_3step-CA'] = $wgGlobalCollectGatewayHtmlFormDir . '/webitects_2_3step-CA.html';
-$wgGlobalCollectGatewayAllowedHtmlForms['webitects_2_3stepB-US'] = $wgGlobalCollectGatewayHtmlFormDir . '/webitects_2_3stepB-US.html';
+// Bank Xfer - Two Step
 $wgGlobalCollectGatewayAllowedHtmlForms['bt'] = $wgGlobalCollectGatewayHtmlFormDir . '/bt/bt.html';
 $wgGlobalCollectGatewayAllowedHtmlForms['bt-CA'] = $wgGlobalCollectGatewayHtmlFormDir . '/bt/bt-CA.html';
 $wgGlobalCollectGatewayAllowedHtmlForms['bt-US'] = $wgGlobalCollectGatewayHtmlFormDir . '/bt/bt-US.html';
-$wgGlobalCollectGatewayAllowedHtmlForms['webitects2nd'] = $wgGlobalCollectGatewayHtmlFormDir . '/webitects2nd.html';
-$wgGlobalCollectGatewayAllowedHtmlForms['rtbt-enets'] = $wgGlobalCollectGatewayHtmlFormDir . '/rtbt/rtbt-enets.html';
-$wgGlobalCollectGatewayAllowedHtmlForms['rtbt-eps'] = $wgGlobalCollectGatewayHtmlFormDir . '/rtbt/rtbt-eps.html';
-$wgGlobalCollectGatewayAllowedHtmlForms['rtbt-ideal'] = $wgGlobalCollectGatewayHtmlFormDir . '/rtbt/rtbt-ideal.html';
+
+// RealTime Banking - Two Step
 $wgGlobalCollectGatewayAllowedHtmlForms['rtbt-sofo'] = $wgGlobalCollectGatewayHtmlFormDir . '/rtbt/rtbt-sofo.html';
 $wgGlobalCollectGatewayAllowedHtmlForms['rtbt-sofo-GB'] = $wgGlobalCollectGatewayHtmlFormDir . '/rtbt/rtbt-sofo-GB.html';
+$wgGlobalCollectGatewayAllowedHtmlForms['rtbt-ideal'] = $wgGlobalCollectGatewayHtmlFormDir . '/rtbt/rtbt-ideal.html';
+$wgGlobalCollectGatewayAllowedHtmlForms['rtbt-enets'] = $wgGlobalCollectGatewayHtmlFormDir . '/rtbt/rtbt-enets.html';
+$wgGlobalCollectGatewayAllowedHtmlForms['rtbt-eps'] = $wgGlobalCollectGatewayHtmlFormDir . '/rtbt/rtbt-eps.html';
+$wgGlobalCollectGatewayAllowedHtmlForms['rtbt-ideal-noadd'] = $wgGlobalCollectGatewayHtmlFormDir . '/rtbt/rtbt-ideal-noadd.html';
+
+// Credit Card - Single Step
+//$wgGlobalCollectGatewayAllowedHtmlForms['webitects_2_3step'] = $wgGlobalCollectGatewayHtmlFormDir . '/webitects_2_3step.html';
+$wgGlobalCollectGatewayAllowedHtmlForms['webitects_2_3step-CA'] = $wgGlobalCollectGatewayHtmlFormDir . '/webitects_2_3step-CA.html';
+$wgGlobalCollectGatewayAllowedHtmlForms['webitects_2_3step-US'] = $wgGlobalCollectGatewayHtmlFormDir . '/webitects_2_3step-US.html';
+$wgGlobalCollectGatewayAllowedHtmlForms['webitects_2_3stepB-US'] = $wgGlobalCollectGatewayHtmlFormDir . '/webitects_2_3stepB-US.html';
+
+// Credit Card - Two Step
+$wgGlobalCollectGatewayAllowedHtmlForms['webitects2nd'] = $wgGlobalCollectGatewayHtmlFormDir . '/webitects2nd.html';
+$wgGlobalCollectGatewayAllowedHtmlForms['webitects2nd-US'] = $wgGlobalCollectGatewayHtmlFormDir . '/webitects2nd-US.html';
 
 /**
  * PayflowPro RapidHTML whitelist additions
  */
 //The following example is hard-coded as a default. Just a sample line. 
 //$wgPayflowProGatewayAllowedHtmlForms['lightbox1'] = $wgPayflowProGatewayHtmlFormDir .'/lightbox1.html';
-$wgPayflowProGatewayAllowedHtmlForms['webitects_2_3step'] = $wgPayflowProGatewayHtmlFormDir .'/webitects_2_3step.html';
+// Credit Card - Single Step
+//$wgPayflowProGatewayAllowedHtmlForms['webitects_2_3step'] = $wgPayflowProGatewayHtmlFormDir .'/webitects_2_3step.html';
 $wgPayflowProGatewayAllowedHtmlForms['webitects_2_3step-CA'] = $wgPayflowProGatewayHtmlFormDir .'/webitects_2_3step-CA.html';
+
+// Credit Card - Two Step
 $wgPayflowProGatewayAllowedHtmlForms['webitects_2_2step-US'] = $wgPayflowProGatewayHtmlFormDir .'/webitects_2_2step-US.html';
 $wgPayflowProGatewayAllowedHtmlForms['webitects_2_2stepB-US'] = $wgPayflowProGatewayHtmlFormDir .'/webitects_2_2stepB-US.html';
-$wgPayflowProGatewayAllowedHtmlForms['globalcollect_test_2'] = $donationinterface_dir .'/gateway_forms/rapidhtml/html/globalcollect_test_2.html';
