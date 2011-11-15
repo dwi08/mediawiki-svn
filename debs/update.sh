07428d98b07c7ebe54275356d334ca0f1a756bb9
hashar <hashar@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-11-15 21:52:57 +0000

    debs: comments to update.sh
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@103231 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- debs/update.sh
+++ debs/update.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+#
+# This script is used to update a .deb package from the subversion repository
+# and then update the WMF apt repository.
+#
+# Usage: update.sh <package>
+#
+# Author: Tim Starling 2007,2008,2010
 
+# Exit immediately if a command exits with a non-zero status
 set -e
 
+# We do require a package name as argument
 if [ -z $1 ];then
 	echo "Usage: update.sh <package>"
 fi
