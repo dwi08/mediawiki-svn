8914f889b8757b5c61b98df30b24fbcbecb15d45
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-11-15 01:39:45 +0000

    stub loading content via API -- this gives regular view and has no frame security
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@103117 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- mockups/athena/athena.js
+++ mockups/athena/athena.js
@@ -1,2 +1,79 @@
 // stub
+(function($) {
+
+var wiki = {
+	base: 'https://$1.wikipedia.org/w/api.php',
+	lang: 'en',
+
+	/**
+	 * @return string
+	 */
+	apiUrl: function() {
+		return wiki.base.replace('$1', wiki.lang);
+	},
+
+	/**
+	 * @param object params: map of parameters to send to API
+	 * @return promise
+	 */
+	api: function(params) {
+		var ajaxParams = {
+			format: 'json'
+		};
+		$.extend(ajaxParams, params || {});
+		return $.ajax({
+			url: wiki.apiUrl(),
+			data: ajaxParams,
+			dataType: 'jsonp'
+		});
+	}
+};
+
+var ui = {
+	startSpinner: function() {
+	},
+	
+	stopSpinner: function() {
+	},
+	
+	showPage: function(title, content) {
+		// @fixme trust issues :)
+		console.log('Showing title', title);
+		console.log('Showing content', content);
+		$('#page-content').html(content);
+	}
+};
+
+var app = {
+	loadPage: function(title) {
+		ui.startSpinner();
+		wiki.api({
+			action: 'query',
+			prop: 'revisions',
+			titles: title,
+			rvprop: 'timestamp|content',
+			rvparse: 1
+		}).then(function(data) {
+			console.log('page!', data);
+			var pageId, page;
+			$.each(data.query.pages, function() {
+				pageId = this.id;
+				page = this;
+			});
+			var revId, rev;
+			$.each(page.revisions, function() {
+				revId = this.revid;
+				rev = this;
+			});
+			ui.showPage(page.title, rev['*']);
+			ui.stopSpinner();
+		});
+	},
+};
+
+$(function() {
+	app.loadPage('While My Guitar Gently Weeps');
+});
+
+})(jQuery);
 
