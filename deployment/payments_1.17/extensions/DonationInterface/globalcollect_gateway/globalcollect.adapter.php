3d7c05a570b4f80207aca40c1596a6f3673f4eea
khorn <khorn@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-11-19 00:47:55 +0000

    Deploy-only change, for the lightweight variable constraint system (that will go away as soon as we rejoin the real world in Dev).
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/fundraising@103630 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- deployment/payments_1.17/extensions/DonationInterface/globalcollect_gateway/globalcollect.adapter.php
+++ deployment/payments_1.17/extensions/DonationInterface/globalcollect_gateway/globalcollect.adapter.php
@@ -1536,6 +1536,7 @@ class GlobalCollectAdapter extends GatewayAdapter {
 			'street',
 			'city',
 			'state',
+			'zip',
 			'email',
 			'amount',
 			'card_type',
@@ -1572,6 +1573,11 @@ class GlobalCollectAdapter extends GatewayAdapter {
 	protected function stage_state( $type = 'request' ) {
 		// Truncate to 35 characters due to GlobalCollect's field length limit
 		$this->staged_data['state'] = substr( $this->staged_data['state'], 0, 35 );
+	}
+	
+	protected function stage_zip( $type = 'request' ) {
+		// Truncate to 10 characters due to GlobalCollect's field length limit
+		$this->staged_data['zip'] = substr( $this->staged_data['zip'], 0, 10 );
 	}
 	
 	protected function stage_email( $type = 'request' ) {
