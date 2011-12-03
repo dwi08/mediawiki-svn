eb760eacc6f109c5949f86f9ad2c8edbdcd2f200
aaron <aaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-03 19:30:21 +0000

    Added the first FileBackends tests
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/FileBackend@105077 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phase3/tests/phpunit/includes/filerepo/FileBackendTest.php
+++ phase3/tests/phpunit/includes/filerepo/FileBackendTest.php
@@ -1,20 +1,146 @@
 <?php
 
-
 class FileBackendTest extends MediaWikiTestCase {
+	private $backend, $filesToPrune, $pathsToPrune;
 
+	function setUp() {
+		global $IP;
+		parent::setUp();
+		$this->backend = new FSFileBackend( array(
+			'name'        => 'localtesting',
+			'lockManager' => 'fsLockManager',
+			'containerPaths' => array(
+				'cont1' => "$IP/tests/tempimages/cont1",
+				'cont2' => "$IP/tests/tempimages/cont2" )
+		) );
+		$this->filesToPrune = array();
+		$this->pathsToPrune = array();
+	}
 
-    /**
-     *
-     * @dataProvider provideTestFiles
-     */
-    function testStore( $srcPath, $dstZone, $dstRel, $flags ) {
+	private function basePath() {
+		return 'mwstore://localtesting';
+	}
 
-    }
+	/**
+	 * @dataProvider provider_testStore
+	 */
+	public function testStore( $op, $source, $dest ) {
+		$this->filesToPrune[] = $source;
+		$this->pathsToPrune[] = $dest;
 
-    function provideTestFiles() {
-        return array(
-                     array('srcPath', 'dstZone', 'dstRel', 'flags')
-        );
-    }
-} // end class
+		file_put_contents( $source, "Unit test file" );
+		$status = $this->backend->doOperation( $op );
+
+		$this->assertEquals( true, $status->isOK(),
+			"Store from $source to $dest succeeded." );
+		$this->assertEquals( true, $status->isGood(),
+			"Store from $source to $dest succeeded without warnings." );
+		$this->assertEquals( true, file_exists( $source ),
+			"Source file $source still exists." );
+		$this->assertEquals( true, $this->backend->fileExists( array( 'src' => $dest ) ),
+			"Destination file $dest exists." );
+
+		$props1 = FSFile::getPropsFromPath( $source );
+		$props2 = $this->backend->getFileProps( array( 'src' => $dest ) );
+		$this->assertEquals( $props1, $props2,
+			"Source and destination have the same props." );
+	}
+
+	public function provider_testStore() {
+		$cases = array();
+
+		$tmpName = TempFSFile::factory( "unittests_", 'txt' )->getPath();
+		$toPath = $this->basePath() . '/cont1/fun/obj1.txt';
+		$op = array( 'op' => 'store', 'src' => $tmpName, 'dst' => $toPath );
+		$cases[] = array(
+			$op, // operation
+			$tmpName, // source
+			$toPath, // dest
+		);
+
+		$op['overwriteDest'] = true;
+		$cases[] = array(
+			$op, // operation
+			$tmpName, // source
+			$toPath, // dest
+		);
+
+		return $cases;
+	}
+
+	/**
+	 * @dataProvider provider_testCopy
+	 */
+	public function testCopy() {
+	
+	}
+
+	/**
+	 * @dataProvider provider_testMove
+	 */
+	public function testMove() {
+	
+	}
+
+	/**
+	 * @dataProvider provider_testDelete
+	 */
+	public function testDelete() {
+	
+	}
+
+	/**
+	 * @dataProvider provider_testCreate
+	 */
+	public function testCreate() {
+	
+	}
+
+	/**
+	 * @dataProvider provider_testConcatenate
+	 */
+	public function testConcatenate() {
+	
+	}
+
+	/**
+	 * @dataProvider provider_testPrepare
+	 */
+	public function testPrepare() {
+	
+	}
+
+	/**
+	 * @dataProvider provider_testSecure
+	 */
+	public function testSecure() {
+	
+	}
+
+	/**
+	 * @dataProvider provider_testClean
+	 */
+	public function testClean() {
+	
+	}
+
+	/**
+	 * @dataProvider provider_testGetLocalCopy
+	 */
+	public function testGetLocalCopy() {
+	
+	}
+
+	function tearDown() {
+		parent::tearDown();
+		foreach ( $this->filesToPrune as $file ) {
+			@unlink( $file );
+		}
+		foreach ( $this->pathsToPrune as $file ) {
+			$this->backend->delete( array( 'src' => $file ) );
+		}
+		$this->backend = null;
+		$this->filesToPrune = array();
+		$this->pathsToPrune = array();
+	}
+}
