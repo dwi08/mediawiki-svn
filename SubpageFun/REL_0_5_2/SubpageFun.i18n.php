b6e4be149877a4b9dd34dd95fb2b62d06c029fbe
danwe <danwe@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-04 23:27:20 +0000

    Tag for 'Subpage Fun' 0.5.2
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/tags/extensions@105138 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ SubpageFun/REL_0_5_2/SubpageFun.i18n.php
@@ -0,0 +1,86 @@
+<?php
+#coding: utf-8
+
+/**
+ * Internationalisation file of the 'Subpage Fun' extension
+ *
+ * @since 0.5
+ * 
+ * @file SubpageFun.i18n.php
+ * @ingroup SubpageFun
+ * @author Daniel Werner < danweetz@web.de >
+ */
+
+$messages = array();
+
+/** English
+ * @author Daniel Werner
+ */
+$messages['en'] = array(
+	'subpagefun-desc' => 'Adds parser functions allowing to gain extensive information about [http://www.mediawiki.org/wiki/Help:Subpages subpages]',
+);
+
+/** Asturian (Asturianu)
+ * @author Xuacu
+ */
+$messages['ast'] = array(
+	'subpagefun-desc' => 'Amiesta funciones analítiques que permiten sacar información abondo tocante a les [http://www.mediawiki.org/wiki/Help:Subpages sub-páxines]',
+);
+
+/** German (Deutsch)
+ * @author Daniel Werner
+ */
+$messages['de'] = array(
+	'subpagefun-desc' => 'Fügt Parser-Funktionen zum Ermitteln umfangreicher Informationen über [http://www.mediawiki.org/wiki/Help:Subpages Unterseiten] hinzu',
+);
+
+/** French (Français)
+ * @author Gomoko
+ */
+$messages['fr'] = array(
+	'subpagefun-desc' => "Ajoute des fonctions d'analyse permettant d'obtenir des informations complètes sur [http://www.mediawiki.org/wiki/Help:Subpages les sous-pages]",
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'subpagefun-desc' => 'Engade funcións analíticas que permiten obter información máis ampla acerca das [http://www.mediawiki.org/wiki/Help:Subpages subpáxinas]',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'subpagefun-desc' => 'Přidawa parserowe funkcije, kotrež dowoleja wobšěrne informacije wo [http://www.mediawiki.org/wiki/Help:Subpages podstronach] dóstać',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'subpagefun-desc' => 'Adde functiones al analysator syntactic que permitte ganiar information extense a proposito de [http://www.mediawiki.org/wiki/Help:Subpages subpaginas]',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'subpagefun-desc' => 'Додава парсерски функции што даваат приказ на опширни информации за [www.mediawiki.org/wiki/Help:Subpages?uselang=mk потстраници]',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'subpagefun-desc' => "Voegt parserfuncties toe die het mogelijk maken informatie over [http://www.mediawiki.org/wiki/Help:Subpages subpagina's] weer te geven",
+);
+
+/** Piedmontese (Piemontèis)
+ * @author Borichèt
+ * @author Dragonòt
+ */
+$messages['pms'] = array(
+	'subpagefun-desc' => "A gionta dle funsion d'anàlisi ch'a përmëtto ëd pijé d'anformassion complete an sle [http://www.mediawiki.org/wiki/Help:Subpages sot-pàgine]",
+);
+
