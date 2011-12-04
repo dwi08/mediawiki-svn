56f410dd46929a51f1057cb38f674713dfc6cb4a
danwe <danwe@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-04 23:38:33 +0000

    Tag for 'SemanticUpdateOnPurge' 0.4.1
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/tags/extensions@105142 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ SemanticUpdateOnPurge/REL_0_4_1/SemanticUpdateOnPurge.i18n.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Internationalisation file of the 'SemanticUpdateOnPurge' extension
+ *
+ * @since 0.4
+ * 
+ * @file SemanticUpdateOnPurge.i18n.php
+ * @ingroup SemanticUpdateOnPurge
+ * @author Daniel Werner < danweetz@web.de >
+ */
+
+$messages = array();
+
+/** English
+ * @author Daniel Werner
+ */
+$messages['en'] = array(
+	'suop-desc' => 'Updates semantic attributes of a wiki page when purging the page',
+);
+
+/** Asturian (Asturianu)
+ * @author Xuacu
+ */
+$messages['ast'] = array(
+	'suop-desc' => "Anueva los atributos semánticos d'una páxina wiki al purgar la páxina",
+);
+
+/** German (Deutsch)
+ * @author Daniel Werner
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'suop-desc' => 'Aktualisiert die semantischen Attribute auf einer Wiki-Seite, wenn deren Seitencache durch die Aktion „purge“ geleert wird',
+);
+
+/** French (Français)
+ * @author Sherbrooke
+ */
+$messages['fr'] = array(
+	'suop-desc' => "Met à jour les attributs sémantiques d'une page wiki quand son cache est purgé",
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'suop-desc' => 'Actualiza os atributos semánticos dunha páxina wiki ao purgar a páxina',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'suop-desc' => 'Aktualizuje semantiske atributy wikistrony, hdyž so pufrowak strony wuprózdnja',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'suop-desc' => 'Actualisa le attributos semantic de un pagina wiki quando le pagina es purgate',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'suop-desc' => "Aktualiséiert déi semantesch Attributer vun enger Wiki-Säit wann d'Säite komplett eidel gemaach gëtt (purging the page)",
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'suop-desc' => 'Ги подновува семантичките атрибути на една вики-страница при нејзино пречистување (на кешот)',
+);
+
+/** Malay (Bahasa Melayu)
+ * @author Anakmalaysia
+ */
+$messages['ms'] = array(
+	'suop-desc' => 'Mengemaskinikan atribut-atribut semantik pada laman wiki sewaktu menyingkirkan laman',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'suop-desc' => 'Werkt semantische eigenschappen van een wikipagina bij als de pagina uit de cache wordt verwijderd',
+);
+
+/** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬)
+ * @author Event
+ */
+$messages['no'] = array(
+	'suop-desc' => 'Oppdater semantiske attributter for en wikiside når siden blir ryddet',
+);
+
