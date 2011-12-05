bea7caf847ecf4920244d750439fd965742a7181
nikerabbit <nikerabbit@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-05 18:38:27 +0000

    MFT r99905 r100958 r101995 r103128 r103168 r103173 r103646 r103802 r103804 r104005 r104006 r104007 r104009 r104592
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf@105202 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- 1.18wmf1/extensions/Narayam/Narayam.php
+++ 1.18wmf1/extensions/Narayam/Narayam.php
@@ -174,7 +174,11 @@ $wgResourceModules['ext.narayam.core'] = $narayamTpl + array(
 		'narayam-bn-inscript',
 		'narayam-bn-nkb',
 	),
-	'dependencies' => array( 'mediawiki.util', 'jquery.textSelection' ),
+	'dependencies' => array(
+		'mediawiki.util',
+		'jquery.textSelection',
+		'jquery.cookie',
+	),
 );
 $wgResourceModules['ext.narayam.rules.am'] = $narayamTpl + array(
 	'scripts' => 'js/ext.narayam.rules.am.js',
