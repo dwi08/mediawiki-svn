d7a768b2ed0fe514f49f9f6d9004a60c179c633c
danwe <danwe@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-08 23:24:38 +0000

    Tag for version 0.4
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/tags/extensions@105609 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ Loops/REL_0_4/Loops.i18n.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Internationalization file of the 'Loops' extension.
+ *
+ * @since 0.4
+ *
+ * @file Loops.i18n.php
+ * @ingroup Loops
+ * @author Daniel Werner < danweetz@web.de >
+ */
+
+$messages = array();
+
+/** English
+ * @author David M. Sledge
+ */
+$messages['en'] = array(
+	'loops-desc' => 'Parser functions for performing loops',
+	'loops_max' => 'Maximum number of loops have been performed'
+);
+
+/** German (Deutsch)
+ * @author Daniel Werner
+ */
+$messages['de'] = array(
+	'loops-desc' => 'Parser-Funktionen zur Schleifen-Ausführung',
+	'loops_max' => 'Die maximal erlaubte Anzahl an Schleifen wurde ausgeführt',
+);
+
+/** French (Français)
+ * @author DavidL
+ */
+$messages['fr'] = array(
+	'loops-desc' => "Fonctions de l'interpréteur pour effectuer des boucles",
+	'loops_max' => 'Le nombre maximal de boucles a été effectué',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'loops-desc' => 'Funcións analíticas para levar a cabo bucles',
+	'loops_max' => 'Levouse a cabo o número máximo de bucles',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'loops-desc' => 'Parserowe funkcije za wuwjedźenje seklow',
+	'loops_max' => 'Maksimalna ličba seklow je so wuwjedła',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'loops-desc' => 'Functiones de analysator syntactic pro executar iterationes',
+	'loops_max' => 'Le numero maxime de iterationes ha essite executate',
+);
+
+/** Japanese (日本語)
+ * @author Fryed-peach
+ */
+$messages['ja'] = array(
+	'loops-desc' => 'ループを実行するためのパーサー関数',
+	'loops_max' => 'ループの最大実行回数に達しました',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'loops-desc' => 'Parser-Fonctioune fir Schleefen auszeféieren',
+	'loops_max' => 'Déi maximal Zuel vun erlaabte Schleefe gouf ausgefouert',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'loops-desc' => 'Парсерски функции за вршење на повторувања (јамки)',
+	'loops_max' => 'Извршен е максималниот број на повторувања',
+);
+
+/** Malay (Bahasa Melayu)
+ * @author Anakmalaysia
+ */
+$messages['ms'] = array(
+	'loops-desc' => 'Fungsi parser untuk melakukan gelung',
+	'loops_max' => 'Bilangan gelung maksimum telah dilakukan',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'loops-desc' => 'Parserfuncties voor het uitvoeren van herhalingen',
+	'loops_max' => 'Het maximum aantal herhalingen is uitgevoerd',
+);
+
