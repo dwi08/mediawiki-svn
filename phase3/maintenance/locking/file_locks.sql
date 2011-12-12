ceea3170f91e856034bf8d5293e779367a0b8848
aaron <aaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-12 00:50:37 +0000

    FU r105819: missing sql file change
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/FileBackend@105845 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phase3/maintenance/locking/file_locks.sql
+++ phase3/maintenance/locking/file_locks.sql
@@ -1,4 +1,10 @@
 -- Create table to handle resource locking
-CREATE TABLE /*$wgDBprefix*/file_locks (
-	fl_key binary(40) NOT NULL default '' PRIMARY KEY
+CREATE TABLE /*_*/file_locks_exclusive (
+	fle_key binary(40) NOT NULL default '' PRIMARY KEY
 ) ENGINE=InnoDB, DEFAULT CHARSET=binary;
+
+-- Create table to handle resource locking
+CREATE TABLE /*_*/file_locks_shared (
+	fls_key binary(40) NOT NULL default ''
+) ENGINE=InnoDB, DEFAULT CHARSET=binary;
+CREATE INDEX /*i*/fls_key ON /*_*/file_locks_shared (fls_key);
