9c536007d27946a87e745acc847b2a833979bc98
aaron <aaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-12 21:24:23 +0000

    Added comments to make this sql file clearer
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/FileBackend@105931 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phase3/maintenance/locking/file_locks.sql
+++ phase3/maintenance/locking/file_locks.sql
@@ -1,9 +1,9 @@
--- Create table to handle resource locking
+-- Table to handle resource locking (EX) with row-level locking
 CREATE TABLE /*_*/file_locks_exclusive (
 	fle_key binary(40) NOT NULL default '' PRIMARY KEY
 ) ENGINE=InnoDB, DEFAULT CHARSET=binary;
 
--- Create table to handle resource locking
+-- Table to handle resource locking (SH) with row-level locking
 CREATE TABLE /*_*/file_locks_shared (
 	fls_key binary(40) NOT NULL default ''
 ) ENGINE=InnoDB, DEFAULT CHARSET=binary;
