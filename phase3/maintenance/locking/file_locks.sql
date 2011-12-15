c1d35e0852835e5d8ed1847aeee4913e8368e98e
aaron <aaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-15 15:48:23 +0000

    Cleaned up file_locks.sql a bit
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/FileBackend@106331 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phase3/maintenance/locking/file_locks.sql
+++ phase3/maintenance/locking/file_locks.sql
@@ -1,11 +1,11 @@
 -- Table to handle resource locking (EX) with row-level locking
 CREATE TABLE /*_*/filelocks_exclusive (
-	fle_key binary(40) NOT NULL default '' PRIMARY KEY
+	fle_key binary(40) NOT NULL PRIMARY KEY
 ) ENGINE=InnoDB, CHECKSUM=0;
 
 -- Table to handle resource locking (SH) with row-level locking
 CREATE TABLE /*_*/filelocks_shared (
-	fls_key binary(40) NOT NULL default '',
-	fls_session integer unsigned NOT NULL default 0,
+	fls_key binary(40) NOT NULL,
+	fls_session integer unsigned NOT NULL,
 	PRIMARY KEY (fls_key,fls_session)
 ) ENGINE=InnoDB, CHECKSUM=0;
