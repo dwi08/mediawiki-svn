cd62624fba5895e3495a38327bc345d48d534041
petrb <petrb@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-21 10:57:51 +0000

    Little update for it to send less output than now
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@106937 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/nparser/fetch.sh
+++ tools/nparser/fetch.sh
@@ -11,9 +11,8 @@ if [ -f parser ];then
                 exit 1
         fi
         mkdir "$temp"
-        wget https://labsconsole.wikimedia.org/wiki/Special:Ask/-5B-5BResource-20Type::instance-5D-5D/-3FInstance-20Name/-3FInstance-20Type/-3FProject/-3FImage-20Id/-3FFQDN/-3FLaunch-20Time/-3FPuppet-20Class/-3FModification-20date/-3FInstance-20Host/-3FNumber-20of-20CPUs/-3FRAM-20Size/-3FAmount-20of-20Storage/limit%3D500/format%3Djson -O $temp/novalist
-        if [ "`diff old/* $temp/*`" = "" ];then
-                echo OK
+        wget https://labsconsole.wikimedia.org/wiki/Special:Ask/-5B-5BResource-20Type::instance-5D-5D/-3FInstance-20Name/-3FInstance-20Type/-3FProject/-3FImage-20Id/-3FFQDN/-3FLaunch-20Time/-3FPuppet-20Class/-3FModification-20date/-3FInstance-20Host/-3FNumber-20of-20CPUs/-3FRAM-20Size/-3FAmount-20of-20Storage/limit%3D500/format%3Djson -O $temp/novalist -o logs
+        if [ "`diff old/* $temp/*`" != "" ];then
         out=`./parser "$temp"`
         if [ `echo $out | grep -c Error` -gt 0 ];then
                 echo Unable to parse the file
@@ -24,7 +23,9 @@ if [ -f parser ];then
         list2=`echo "$out" | awk '{ if (p==1) print $0; p=2 } { if ($0 ~ /SSH:/) p=1 }'`
         for instance in `echo "$instances"`
         do
+                if [ "$instance" != "nagios" ];then
                 cat default.cfg | sed "s/NAME/$instance/" > conf/$instance".cfg"
+                fi
         done
         cp generic/* conf
         cat hostgroup | sed "s/#web/$list1/" | sed "s/#SSH/$list2/" > conf/hostgroups_nagios2.cfg
