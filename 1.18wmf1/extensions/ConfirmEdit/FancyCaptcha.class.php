34986dc313b7bd70e3e743fda6d5a2dc659a1331
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-26 20:01:25 +0000

    MFT r107337
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf@107340 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- 1.18wmf1/extensions/ConfirmEdit/FancyCaptcha.class.php
+++ 1.18wmf1/extensions/ConfirmEdit/FancyCaptcha.class.php
@@ -72,6 +72,8 @@ class FancyCaptcha extends SimpleCaptcha {
 			Xml::element( 'input', array(
 				'name' => 'wpCaptchaWord',
 				'id'   => 'wpCaptchaWord',
+				'autocorrect' => 'off',
+				'autocapitalize' => 'off',
 				'tabindex' => 1 ) ) . // tab in before the edit textarea
 			"</p>\n";
 	}
