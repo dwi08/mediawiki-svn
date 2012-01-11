b53147d969e07eff4e7fce384852a61c56bb2d0c
preilly <preilly@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2012-01-11 00:07:55 +0000

    1.18wmf1: MFT r106907 - r108532
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf@108571 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ 1.18wmf1/extensions/ZeroRatedMobileAccess/ZeroRatedMobileAccess.i18n.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Internationalisation file for the extension ZeroRatedMobileAccess
+ *
+ * @file
+ * @ingroup Extensions
+ * @author Patrick Reilly
+ * @copyright © 2011 Patrick Reilly
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages = array();
+
+/** English
+ * @author Patrick Reilly
+ */
+$messages['en'] = array (
+	'zero-rated-mobile-access-desc' => 'Provides features for mobile providers that provide free access',
+	'zero-rated-mobile-access-search' => 'Search',
+	'zero-rated-mobile-access-language-options-wiki-page' => 'zero-rated-mobile-access-language-options',
+	'zero-rated-mobile-access-carrier-options-wiki-page' => 'zero-rated-mobile-access-carrier-options',
+	'zero-rated-mobile-access-language-selection' => 'All languages',
+	'zero-rated-mobile-access-home-page-selection' => '$1 home page',
+	'zero-rated-mobile-access-home-page-selection-text' => 'Or go to the home page in the following language:',
+	'zero-rated-mobile-access-banner-text' => 'Free Wikipedia from $1',
+	'zero-rated-mobile-access-banner-text-data-charges' => 'Data charges may apply continue: $1 or $2',
+	'zero-rated-mobile-access-banner-text-data-charges-yes' => 'Yes',
+	'zero-rated-mobile-access-banner-text-data-charges-no' => 'No',
+	'zero-rated-mobile-access-click-to-view-image' => 'Click to view image of, "$1"...',
+);
+
+/** Message documentation (Message documentation)
+ * @author Patrick Reilly
+ */
+$messages['qqq'] = array(
+	'zero-rated-mobile-access-desc' => '{{desc}}',
+	'zero-rated-mobile-access-search' => 'Text for search action on the search button',
+	'zero-rated-mobile-access-language-options-wiki-page' => 'The name of the wiki page in the MediaWiki namespace to store the country language data',
+	'zero-rated-mobile-access-language-selection' => 'Text to show the user when they select a language option',
+	'zero-rated-mobile-access-home-page-selection' => 'Name of language followed by text to show user in local link to Wikipedia',
+	'zero-rated-mobile-access-home-page-selection-text' => 'Text to show user prior to selection of language option',
+	'zero-rated-mobile-access-banner-text' => 'Text shown to users of various carriers with carrier name',
+	'zero-rated-mobile-access-banner-text-data-charges' => 'Data charges notice to user before images are displayed
+* $1 = {{Msg-mw|zero-rated-mobile-access-banner-text-data-charges-yes}}
+* $2 = {{Msg-mw|zero-rated-mobile-access-banner-text-data-charges-no}}',
+	'zero-rated-mobile-access-banner-text-data-charges-yes' => 'Text for accepting charges',
+	'zero-rated-mobile-access-banner-text-data-charges-no' => 'Text for rejecting charges',
+	'zero-rated-mobile-access-click-to-view-image' => 'Text for viewing an image link',
+);
+
+/** Japanese (日本語)
+ * @author Patrick Reilly
+ */
+$messages['ja'] = array (
+	'zero-rated-mobile-access-desc' => 'ゼロ格モバイルアクセス',
+	'zero-rated-mobile-access-search' => '検索する',
+);
+
