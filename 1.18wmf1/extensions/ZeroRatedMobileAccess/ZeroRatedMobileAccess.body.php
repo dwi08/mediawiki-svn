01bcd542586754689a818f399e9dd99e3040f5ad
preilly <preilly@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2012-01-11 01:04:59 +0000

    1.18wmf1: MFT r108582
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf@108583 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- 1.18wmf1/extensions/ZeroRatedMobileAccess/ZeroRatedMobileAccess.body.php
+++ 1.18wmf1/extensions/ZeroRatedMobileAccess/ZeroRatedMobileAccess.body.php
@@ -398,7 +398,7 @@ class ExtZeroRatedMobileAccess {
 		// Use the revision directly to prevent other hooks to be called
 		$rev = Revision::newFromTitle( $title );
 		if ( $rev ) {
-			$sha1OfRev = $rev->getSha1();
+			$sha1OfRev = $rev->getSize();
 			$key = wfMemcKey( 'zero-rated-mobile-access-carrier-options', $sha1OfRev );
 			$carrierOptions = $wgMemc->get( $key );
 		} else {
@@ -452,7 +452,7 @@ class ExtZeroRatedMobileAccess {
 		// Use the revision directly to prevent other hooks to be called
 		$rev = Revision::newFromTitle( $title );
 		if ( $rev ) {
-			$sha1OfRev = $rev->getSha1();
+			$sha1OfRev = $rev->getSize();
 			$key = wfMemcKey( 'zero-rated-mobile-access-language-options', $sha1OfRev );
 			$languageOptions = $wgMemc->get( $key );
 		} else {
@@ -806,6 +806,6 @@ HTML;
 	}
 
 	public function getVersion() {
-		return __CLASS__ . ': $Id: ZeroRatedMobileAccess.body.php 108565 2012-01-10 23:42:04Z preilly $';
+		return __CLASS__ . ': $Id: ZeroRatedMobileAccess.body.php 108582 2012-01-11 01:03:51Z preilly $';
 	}
 }
