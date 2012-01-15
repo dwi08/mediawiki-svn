0f9f4643adc267daeeb3c10fab62cab4932a7e39
oren <oren@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2012-01-15 01:46:31 +0000

    fixed portability issue for running tests
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@108947 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- lucene-search-2/test/org/wikimedia/lsearch/test/WikiTestCase.java
+++ lucene-search-2/test/org/wikimedia/lsearch/test/WikiTestCase.java
@@ -1,19 +1,65 @@
 package org.wikimedia.lsearch.test;
 
+import java.io.File;
+
+import junit.framework.TestCase;
+
 import org.wikimedia.lsearch.analyzers.WikiQueryParser;
 import org.wikimedia.lsearch.config.Configuration;
 import org.wikimedia.lsearch.config.GlobalConfiguration;
 
-import junit.framework.TestCase;
-
 public abstract class WikiTestCase extends TestCase {
+	
+	public Configuration getConfig() {
+		return config;
+	}
+
+
+	public void setConfig(final Configuration config) {
+		this.config = config;
+	}
+
+
+	public GlobalConfiguration getGlobal() {
+		return global;
+	}
+
+
+	public void setGlobal(final GlobalConfiguration global) {
+		this.global = global;
+	}
+
+
 	protected Configuration config = null;
+	
 	protected GlobalConfiguration global = null;
 	
+	
 	protected void setUp() throws Exception {
-		if(config == null){
-			Configuration.setConfigFile(System.getProperty("user.dir")+"/test-data/lsearch.conf.test");
-			Configuration.setGlobalConfigUrl("file://"+System.getProperty("user.dir")+"/test-data/lsearch-global.test");
+		if(config == null)
+		{			
+			String winPathFixer; 
+			if(System.getProperty("os.name").startsWith("Windows")){
+				winPathFixer = File.separator;
+			}else{
+				winPathFixer="";
+			}
+				
+			Configuration.setConfigFile(
+					System.getProperty("user.dir")+
+					winPathFixer+
+					File.separator+
+					"test-data"+
+					File.separator+
+					"lsearch.conf.test");
+
+			Configuration.setGlobalConfigUrl(
+					"file://"+
+					winPathFixer+
+					System.getProperty("user.dir")+					
+					File.separator+"test-data"+
+					File.separator+"lsearch-global.test");
+			
 			config = Configuration.open();
 			global = GlobalConfiguration.getInstance();
 			WikiQueryParser.TITLE_BOOST = 2;
