c661deb3711aaeef573fa40db74bf451fdfe037d
oren <oren@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2012-01-23 02:55:43 +0000

    moved testing code to test code branch - to be converted into unit tests + minor improvements to method names
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@109803 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- lucene-search-3/src/main/java/org/wikimedia/lsearch/util/QueryStringMap.java
+++ lucene-search-3/src/main/java/org/wikimedia/lsearch/util/QueryStringMap.java
@@ -26,12 +26,9 @@ package org.wikimedia.lsearch.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URLDecoder;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 /**
@@ -77,43 +74,4 @@ public class QueryStringMap extends LinkedHashMap<String, Object> implements
 		}
 	}
 
-	public static void main(String[] args) {
-		try {
-			testURI("/x");
-			testURI("/x?foo=bar");
-			testURI("/x?foo=bar&biz=bax");
-
-			// The %26 should _not_ split 'foo' from 'bogo'
-			testURI("/x?foo=bar+%26bogo&next=extreme");
-
-			// UTF-8 good encoding
-			testURI("/x?serveuse=%c3%a9nid");
-
-			// bad encoding; you'll see replacement char
-			testURI("/x?serveuse=%e9nid");
-
-			// corner cases; missing params
-			testURI("/x?foo");
-			testURI("/x?foo&bar=baz");
-			testURI("/x?foo&&bar");
-			testURI("/x?&");
-			testURI("/x?=");
-			testURI("/x?==&");
-
-			testURI("/updatePage?db=wikilucene&namespace=6&title=Nick+Gorton5+8+05.jpg");
-			testURI("/getStatus");
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private static void testURI(String uri) throws URISyntaxException {
-		QueryStringMap map = new QueryStringMap(new URI(uri));
-		System.out.println(uri);
-		Set<String> keys = map.keySet();
-		for (Iterator<String> i = keys.iterator(); i.hasNext();) {
-			String key = i.next();
-			System.out.println("  \"" + key + "\" => \"" + map.get(key) + "\"");
-		}
-	}
 }
