c768c8283610219702157b3cc14f171d163a61ad
oren <oren@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2012-01-23 12:50:47 +0000

    Since these class could not inherit the missing parts  introduced from original implementation
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@109819 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- lucene-search-3/src/main/java/org/apache/lucene/search/PositionalQuery.java
+++ lucene-search-3/src/main/java/org/apache/lucene/search/PositionalQuery.java
@@ -1,10 +1,12 @@
 package org.apache.lucene.search;
 
 import java.io.IOException;
+import java.util.Vector;
 
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.index.TermPositions;
+import org.apache.lucene.util.ToStringUtils;
 
 /**
  * Phrase query with 
@@ -14,7 +16,35 @@ import org.apache.lucene.index.TermPositions;
  * @author rainman
  *
  */
-public class PositionalQuery extends PhraseQuery {
+public class PositionalQuery extends Query {
+
+	  private String field;
+	  private Vector<Term> terms = new Vector<Term>();
+	  private Vector<Integer> positions = new Vector<Integer>();
+	  private int slop = 0;
+	  
+	  /** Sets the number of other words permitted between words in query phrase.
+	    If zero, then this is an exact phrase search.  For larger values this works
+	    like a <code>WITHIN</code> or <code>NEAR</code> operator.
+
+	    <p>The slop is in fact an edit-distance, where the units correspond to
+	    moves of terms in the query phrase out of position.  For example, to switch
+	    the order of two words requires two moves (the first move places the words
+	    atop one another), so to permit re-orderings of phrases, the slop must be
+	    at least two.
+
+	    <p>More exact matches are scored higher than sloppier matches, thus search
+	    results are sorted by exactness.
+
+	    <p>The slop is zero by default, requiring exact matches.*/
+	  public void setSlop(int s) { slop = s; }
+	  /** Returns the slop.  See setSlop(). */
+	  public int getSlop() { return slop; }
+	  
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3882888014364425710L;
 	protected PositionalOptions options; 
 	protected int stopWordCount = 0;
 	
@@ -22,6 +52,37 @@ public class PositionalQuery extends PhraseQuery {
 		this.options = options;
 	}
 
+	  /**
+	   * Adds a term to the end of the query phrase.
+	   * The relative position of the term is the one immediately after the last term added.
+	   */
+	  public void add(Term term) {
+	    int position = 0;
+	    if(positions.size() > 0)
+	        position = positions.lastElement().intValue() + 1;
+
+	    add(term, position);
+	  }
+	  
+	  /**
+	   * Adds a term to the end of the query phrase.
+	   * The relative position of the term within the phrase is specified explicitly.
+	   * This allows e.g. phrases with more than one term at the same position
+	   * or phrases with gaps (e.g. in connection with stopwords).
+	   * 
+	   * @param term
+	   * @param position
+	   */
+	  public void add(Term term, int position) {
+	      if (terms.size() == 0)
+	          field = term.field();
+	      else if (term.field() != field)
+	          throw new IllegalArgumentException("All phrase terms must be in the same field: " + term);
+
+	      terms.addElement(term);
+	      positions.addElement(new Integer(position));
+	  }
+	  
 	/** Add to end of phrase */
 	public void add(Term term, boolean isStopWord) {
 		if(isStopWord)
@@ -29,6 +90,8 @@ public class PositionalQuery extends PhraseQuery {
 		add(term);
 	}
 
+	
+	
 	/** Add to position in phrase */
 	public void add(Term term, int position, boolean isStopWord) {
 		if(isStopWord)
@@ -36,14 +99,49 @@ public class PositionalQuery extends PhraseQuery {
 		add(term,position);
 	}
 	
-	public String toString(String f) {
-		String s = super.toString(f);
-		return "(P "+s+")";
-	}
+
+	  /** Prints a user-readable version of this query. */
+	  public String toString(String f) {
+	    StringBuffer buffer = new StringBuffer();
+	    if (!field.equals(f)) {
+	      buffer.append(field);
+	      buffer.append(":");
+	    }
+
+	    buffer.append("\"");
+	    for (int i = 0; i < terms.size(); i++) {
+	      buffer.append(terms.elementAt(i).text());
+	      if (i != terms.size()-1)
+	  buffer.append(" ");
+	    }
+	    buffer.append("\"");
+
+	    if (slop != 0) {
+	      buffer.append("~");
+	      buffer.append(slop);
+	    }
+
+	    buffer.append(ToStringUtils.boost(getBoost()));
+
+	    return "(P "+buffer.toString()+")";
+	  }
+	
+
 	
 	protected Weight createWeight(Searcher searcher) throws IOException {
 		return new PositionalWeight(searcher);
 	}
+	
+
+	  /**
+	   * Returns the relative positions of terms in this phrase.
+	   */
+	  public int[] getPositions() {
+	      int[] result = new int[positions.size()];
+	      for(int i = 0; i < positions.size(); i++)
+	          result[i] = positions.elementAt(i).intValue();
+	      return result;
+	  }
 	
 	/**
 	 * Weight
@@ -51,10 +149,32 @@ public class PositionalQuery extends PhraseQuery {
 	 * @author rainman
 	 *
 	 */
-	protected class PositionalWeight extends PhraseWeight{		
+	protected class PositionalWeight implements Weight//PhraseWeight
+	{		
+		
+	    private Similarity similarity;
+	    private float value;
+	    private float idf;
+	    private float queryNorm;
+	    private float queryWeight;
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 8588472089439448361L;
+
+		
 		public PositionalWeight(Searcher searcher) throws IOException{
-			super(searcher);
+		      this.similarity = getSimilarity(searcher);
+
+		      idf = similarity.idf(terms, searcher);
 		}
+		
+	    public void normalize(float queryNorm) {
+	        this.queryNorm = queryNorm;
+	        queryWeight *= queryNorm;                   // normalize query weight
+	        value = queryWeight * idf;                  // idf for document 
+	      }
 		
 		public Scorer scorer(IndexReader reader) throws IOException {
 			if (terms.size() == 0)			  // optimize zero-term case
@@ -62,7 +182,7 @@ public class PositionalQuery extends PhraseQuery {
 						
 			TermPositions[] tps = new TermPositions[terms.size()];
 			for (int i = 0; i < terms.size(); i++) {
-				TermPositions p = reader.termPositions((Term)terms.elementAt(i));
+				TermPositions p = reader.termPositions(terms.elementAt(i));
 				if (p == null)
 					return null;
 				tps[i] = p;
@@ -101,7 +221,7 @@ public class PositionalQuery extends PhraseQuery {
 	          query.append(" ");
 	        }
 
-	        Term term = (Term)terms.elementAt(i);
+	        Term term = terms.elementAt(i);
 
 	        docFreqs.append(term.text());
 	        docFreqs.append("=");
@@ -163,6 +283,13 @@ public class PositionalQuery extends PhraseQuery {
 	        return fieldExpl;
 
 	      return result;
+	    }
+	    public Query getQuery() { return PositionalQuery.this; }
+	    public float getValue() { return value; }
+
+	    public float sumOfSquaredWeights() {
+	      queryWeight = idf * getBoost();             // compute query weight
+	      return queryWeight * queryWeight;           // square it
 	    }
 	}
 
