d7bab576c7f6dd3b585aa608b6bff21d5734c4f3
oren <oren@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2012-01-23 22:42:16 +0000

    converted testing code from main to unit tests
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@109862 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- lucene-search-3/src/test/java/org/wikimedia/lsearch/util/QueryStringMapTest.java
+++ lucene-search-3/src/test/java/org/wikimedia/lsearch/util/QueryStringMapTest.java
@@ -3,6 +3,8 @@
  */
 package org.wikimedia.lsearch.util;
 
+import static org.junit.Assert.assertEquals;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Iterator;
@@ -17,38 +19,167 @@ import org.junit.Test;
 public class QueryStringMapTest {
 
 	@Test
-	public void Test1() throws URISyntaxException {
-
-		// TODO convert main below to Junit Tests
+	public void JustAPath() throws URISyntaxException {
+		
+		String uri="/x";
+		QueryStringMap map = new QueryStringMap(new URI(uri));
+		assertEquals(0, map.size());
+	}
+	
+	@Test
+	public void SimpleQuery01() throws URISyntaxException {
+		
+		String uri="/x?foo=bar";
+		QueryStringMap map = new QueryStringMap(new URI(uri));
+		assertEquals(1, map.size());
+		assertEquals("bar", map.get("foo"));
 
 	}
 
+	@Test
+	public void SimpleQuery02() throws URISyntaxException {
+
+		String uri = "/x?foo=bar&biz=bax";
+		QueryStringMap map = new QueryStringMap(new URI(uri));
+		assertEquals(2, map.size());
+		assertEquals("bar", map.get("foo"));
+		assertEquals("bax", map.get("biz"));
+
+	}
+
+	/**
+	 * The %26 should _not_ split 'foo' from 'bogo'
+	 * 
+	 * @throws URISyntaxException
+	 */
+	@Test
+	public void SpacedParamQuery01() throws URISyntaxException {
+
+		String uri = "/x?foo=bar+%26bogo&next=extreme";
+		QueryStringMap map = new QueryStringMap(new URI(uri));
+		assertEquals((int) 2, map.size());
+		assertEquals("bar &bogo", map.get("foo"));
+		assertEquals("extreme", map.get("next"));
+
+	}
+
+	/**
+	 * UTF-8 good encoding
+	 * 
+	 * @throws URISyntaxException
+	 */
+	@Test
+	public void UTF8EncodingQuery01() throws URISyntaxException {
+
+		String uri = "/x?serveuse=%c3%a9nid";
+		QueryStringMap map = new QueryStringMap(new URI(uri));
+		assertEquals((int) 1, map.size());
+		assertEquals("énid", map.get("serveuse"));
+	}
+
+	/**
+	 * UTF-8 bad encoding
+	 * 
+	 * @throws URISyntaxException
+	 */
+	@Test
+	public void BadUTF8EncodingQuery01() throws URISyntaxException {
+
+		String uri = "/x?serveuse=%e9nid";
+		QueryStringMap map = new QueryStringMap(new URI(uri));
+		assertEquals((int) 1, map.size());
+		assertEquals("�nid", map.get("serveuse"));
+	}
+
+	/**
+	 * missing params
+	 * 
+	 * @throws URISyntaxException
+	 */
+	@Test
+	public void CornerCase01() throws URISyntaxException {
+
+		String uri = "/x?foo";
+		QueryStringMap map = new QueryStringMap(new URI(uri));
+		assertEquals((int) 1, map.size());
+		assertEquals("", map.get("foo"));
+	}
+
+	@Test
+	public void CornerCase02() throws URISyntaxException {
+
+		String uri = "/x?foo&bar=baz";
+		QueryStringMap map = new QueryStringMap(new URI(uri));
+		assertEquals((int) 2, map.size());
+		assertEquals("", map.get("foo"));
+		assertEquals("baz", map.get("bar"));
+	}
+
+	@Test
+	public void CornerCase03() throws URISyntaxException {
+
+		String uri = "/x?foo&&bar";
+		QueryStringMap map = new QueryStringMap(new URI(uri));
+		assertEquals((int) 2, map.size());
+		assertEquals("", map.get("foo"));
+		assertEquals("", map.get("bar"));
+	}
+
+	@Test
+	public void CornerCase04() throws URISyntaxException {
+
+		String uri = "/x?&";
+		QueryStringMap map = new QueryStringMap(new URI(uri));
+		assertEquals(0, map.size());
+	}
+
+	@Test
+	public void CornerCase05() throws URISyntaxException {
+
+		String uri = "/x?=";
+		QueryStringMap map = new QueryStringMap(new URI(uri));
+		assertEquals((int) 0, map.size());
+	}
+
+	@Test
+	public void CornerCase06() throws URISyntaxException {
+
+		String uri = "/x?==&";
+		QueryStringMap map = new QueryStringMap(new URI(uri));
+		assertEquals(0, map.size());
+	}
+
+	@Test
+	public void RealQuery01() throws URISyntaxException {
+
+		String uri = "/updatePage?db=wikilucene&namespace=6&title=Nick+Gorton5+8+05.jpg";
+		QueryStringMap map = new QueryStringMap(new URI(uri));
+		assertEquals((int) 3, map.size());
+		assertEquals("wikilucene", map.get("db"));
+		assertEquals("6", map.get("namespace"));
+		assertEquals("Nick Gorton5 8 05.jpg", map.get("title"));
+	}
+	
+	@Test
+	public void RealQuery02() throws URISyntaxException {
+
+		String uri = "/getStatus";
+		QueryStringMap map = new QueryStringMap(new URI(uri));
+		assertEquals((int) 0, map.size());
+	}
+
+	
+	/**
+	 * little debugger for query strings.
+	 * result is sent to console
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 
 		try {
-			testURI("/x");
-			testURI("/x?foo=bar");
-			testURI("/x?foo=bar&biz=bax");
-
-			// The %26 should _not_ split 'foo' from 'bogo'
-			testURI("/x?foo=bar+%26bogo&next=extreme");
-
-			// UTF-8 good encoding
-			testURI("/x?serveuse=%c3%a9nid");
-
-			// bad encoding; you'll see replacement char
-			testURI("/x?serveuse=%e9nid");
-
-			// corner cases; missing params
-			testURI("/x?foo");
-			testURI("/x?foo&bar=baz");
-			testURI("/x?foo&&bar");
-			testURI("/x?&");
-			testURI("/x?=");
-			testURI("/x?==&");
-
 			testURI("/updatePage?db=wikilucene&namespace=6&title=Nick+Gorton5+8+05.jpg");
-			testURI("/getStatus");
+
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		}
