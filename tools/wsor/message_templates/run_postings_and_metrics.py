8076ebfb3da64354c50516611834bc9c88eeddcd
rfaulk <rfaulk@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2012-01-31 00:17:00 +0000

    modified get_experiment() to more fully define experiments
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@110347 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/wsor/message_templates/run_postings_and_metrics.py
+++ tools/wsor/message_templates/run_postings_and_metrics.py
@@ -37,27 +37,29 @@ def main(args):
                         70 : False, 71 : False, 72 : False, 73 : False, 74 : False, 75 : False, 76 : False, 77 : False,
                         84 : False, 85 : False, 86 : False, 99 : False, 100 : False, 101 : False, 102 : False, 103 : False, 104 : False, 
                         105 : False, 106 : False, 107 : False, 108 : False, 109 : False, 110 : False, 111 : False, 112 : False,  113 : False, 
-                        114 : False, 115 : False, 116 : False, 78 : False, 79 : False, 81 : False, 82 : False, 87 : False, 88 : False, 
+                        114 : False, 115 : False, 116 : False, 78 : False, 79 : False, 81 : False, 82 : False, 87 : True, 88 : True, 
                         89 : True, 90 : True, 91 : True, 92 : True, 93 : True, 94 : True, 95 : True, 96 : True, 97 : True, 98 : True
                         }    
-    
+#    template_indices = {78 : True}
     # Run postings and metrics
     
     generator = 'editcount'
-    postings_cmd = './postings -h db1047 --start=%(start_time)s --end=%(end_time)s --comment="\(\[\[WP:HG\|HG\]\]\)" --message="{{%(template)s}}" --outfilename postings_%(file_name)s.tsv'
+        
+    postings_cmd = './postings -h db1047 --start=%(start_time)s --end=%(end_time)s --comment="%(rev_comment)s" --message="{{%(template)s}}" --outfilename postings_%(file_name)s.tsv'
     metrics_cmd = 'cat ./output/postings_%(file_name)s.tsv | ./metrics -h db1047 --header --outfilename metrics_%(file_name)s_%(fname_generator)s.tsv %(generator)s'
     
     for key in template_indices:   
         
-        name, start_ts, end_ts = get_experiment(key)
+        name, start_ts, end_ts, comment = get_experiment(key)
         template_name = 'z' + str(key)
         
         if template_indices[key]:
             
             logging.info('Generating postings for %s' % template_name)
-            filename_part = start_ts[4:8] + '_' + end_ts[4:8] + '_' + template_name
+            # filename_part = start_ts[4:8] + '_' + end_ts[4:8] + '_' + template_name
+            filename_part = 'test_' + start_ts[4:8] + '_' + end_ts[4:8] + '_' + template_name
 
-            os.system(postings_cmd % {'start_time' : start_ts, 'end_time' : end_ts, 'template' : template_name, 'file_name' : filename_part})
+            os.system(postings_cmd % {'start_time' : start_ts, 'end_time' : end_ts, 'template' : template_name, 'file_name' : filename_part, 'rev_comment' : comment})
             # os.system(metrics_cmd % {'file_name' : filename_part, 'file_name' : filename_part, 'generator' : generator, 'fname_generator' : generator})
         else:
             logging.info('Skipping postings for %s' % template_name)
@@ -70,18 +72,45 @@ def main(args):
 def get_experiment(index):
     
     if index >= 60 and index <= 77:
-        return 'Huggle_3', '20111018000000', '20111119000000'
-    elif index >= 1 and index <= 3:
-        return 'Huggle_1_Portuguese', '20111027000000', '20111128000000'
-    elif (index >= 84 and index <= 86) or (index >= 99 and index <= 106):
-        return 'Huggle_Short_1_and_2', '20111108000000', '20111202000000'
-    elif index >= 107 and index <= 116:
-        return 'Huggle_Short_2', '20111122000000', '20111222000000'
-    elif (index >= 77 and index <= 79) or (index >= 81 and index <= 82):
-        return 'Twinkle_1', '20111109000000', '20111209000000'
-    elif index >= 87 and index <= 98:
-        return 'XLinkBot', '20111117000000', '20111217000000'
+        test_handle = 'Huggle_3'
+        start_ts = '20111018000000'
+        end_ts = '20111119000000'
+        comment = '\(\[\[WP:HG\|HG\]\]\)'
 
+    elif index >= 1 and index <= 3:
+        test_handle = 'Huggle_1_Portuguese'
+        start_ts = '20111027000000'
+        end_ts = '20111128000000'
+        comment = '\(\[\[WP:HG\|HG\]\]\)'
+
+    elif (index >= 84 and index <= 86) or (index >= 99 and index <= 106):
+        test_handle = 'Huggle_Short_1_and_2'
+        start_ts = '20111108000000'
+        end_ts = '20111202000000'
+        comment = '\(\[\[WP:HG\|HG\]\]\)'
+        
+    elif index >= 107 and index <= 116:
+        test_handle = 'Huggle_Short_2'
+        start_ts = '20111122000000'
+        end_ts = '20111222000000'
+        comment = '\(\[\[WP:HG\|HG\]\]\)'
+        
+    elif (index >= 78 and index <= 79) or (index >= 81 and index <= 82):
+        test_handle = 'Twinkle_1'
+        start_ts = '20111109000000'
+        end_ts = '20111209000000'
+        comment = '\(\[\[WP:TW\|TW\]\]\)'
+
+    elif index >= 87 and index <= 98:
+        test_handle = 'XLinkBot'
+        start_ts = '20111117000000'
+        end_ts = '20111217000000'
+        comment = 'BOT'
+
+    logging.info('Processing %(test_handle)s from %(start_ts)s to %(end_ts)s on comment "%(comment)s" ...' % {'test_handle' : test_handle, 'start_ts' : start_ts, 'end_ts' : end_ts, 'comment' : comment})
+   
+    return test_handle, start_ts, end_ts, comment
+    
 """
     Call main, exit when execution is complete
 
