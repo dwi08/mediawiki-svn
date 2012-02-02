12f873d23b60929f82c877f98b70fd3d57cd9f4b
foxtrott <foxtrott@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2012-02-02 20:03:36 +0000

    release version 0.1
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/tags/extensions@110605 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ SemanticPageSeries/REL_0_1/includes/iterators/SPSDateIterator.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * File holding the SPSDateIterator class
+ * 
+ * @author Stephan Gambke
+ * @file
+ * @ingroup SemanticPageSeries
+ */
+if ( !defined( 'SPS_VERSION' ) ) {
+	die( 'This file is part of the SemanticPageSeries extension, it is not a valid entry point.' );
+}
+
+/**
+ * The SPSDateIterator class.
+ *
+ * @ingroup SemanticPageSeries
+ */
+class SPSDateIterator extends SPSIterator {
+	
+	/**
+	 * @return array An array containing the names of the parameters this iterator uses.
+	 */
+	function getParameterNames() {
+		return array('start', 'end', 'period', 'unit');
+	}
+	
+	/**
+	 * @return an array of the values to be used in the target field of the target form
+	 */
+	function getValues ( &$data ){
+		
+		//prepare params for getDatesForRecurringEvent
+		$params = array (
+			'property=SomeDummyProperty',
+			'start=' . $data['start'],
+			'end=' . $data['end'],
+			'period=' . $data['period'],
+			'unit=' . $data['unit'],
+			);
+
+		$values = SMWSetRecurringEvent::getDatesForRecurringEvent($params);
+
+		// if the first date did not contain a time, remove the time from all
+		// generated dates
+		if ( preg_match( '/.:../', $values[1][0] ) === 0 ) {
+			foreach ( $values[1] as $key => $value ) {
+				$values[1][$key] = trim( preg_replace( '/..:..:../', '', $value ) );
+			}
+		}
+
+		return $values[1];
+	}
+}
