f4dec778624112d03cdfe01b5dc85b99bd73134d
khorn <khorn@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2012-02-07 02:23:33 +0000

    MFT  r109685, r110438, r110440, r110448, r110736, r110773, r110800, r110803
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/fundraising@110819 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- deployment/payments_1.17/extensions/DonationInterface/gateway_forms/RapidHtml.php
+++ deployment/payments_1.17/extensions/DonationInterface/gateway_forms/RapidHtml.php
@@ -22,6 +22,7 @@ class Gateway_Form_RapidHtml extends Gateway_Form {
 		'@fname', // => $wgRequest->getText( 'fname' ),
 		'@mname', // => $wgRequest->getText( 'mname' ),
 		'@lname', // => $wgRequest->getText( 'lname' ),
+		'@street_supplemental', // => $wgRequest->getText( 'street_supplemental' ), MUST BE BEFORE @street
 		'@street', // => $wgRequest->getText( 'street' ),
 		'@city', // => $wgRequest->getText( 'city' ),
 		'@state', // => $wgRequest->getText( 'state' ),
