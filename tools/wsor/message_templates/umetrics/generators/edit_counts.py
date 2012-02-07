2ecc7d095b38768c9998b2ac319b79f76ecc3cde
rfaulk <rfaulk@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2012-02-07 02:29:18 +0000

    Added logic to break out edit activity by: 0-3 days, 3-30 days, and more than 30 days after template posting event
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@110820 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/wsor/message_templates/umetrics/generators/edit_counts.py
+++ tools/wsor/message_templates/umetrics/generators/edit_counts.py
@@ -1,4 +1,4 @@
-import itertools
+import itertools, datetime, time
 from .metric_generator import MetricGenerator 
 
 class EditCounts(MetricGenerator):
@@ -10,9 +10,13 @@ class EditCounts(MetricGenerator):
 		return itertools.chain(*[
 			[
 				'ns_%s_revisions_before' % ns,
-				'ns_%s_revisions_after' % ns,
+				'ns_%s_revisions_after_0_3' % ns,
+				'ns_%s_revisions_after_3_30' % ns,
+				'ns_%s_revisions_after_gt_30' % ns,
 				'ns_%s_revisions_deleted_before' % ns,
-				'ns_%s_revisions_deleted_after' % ns
+				'ns_%s_revisions_deleted_after_0_3' % ns,
+				'ns_%s_revisions_deleted_after_3_30' % ns,
+				'ns_%s_revisions_deleted_after_gt_30' % ns
 			]
 			for ns in [0,3]
 			# for ns in itertools.chain(range(0,16), [100, 101, 108, 109])
@@ -21,12 +25,26 @@ class EditCounts(MetricGenerator):
 	def values(self, username, timestamp):
 		rowData = {}
 		
+		# Partition timestamps into:
+		#
+		#	0-3 days after
+		#	3-30 days after
+		#	more than 30 days after
+		
+		timeobj = datetime.datetime(year=int(timestamp[:4]), month=int(timestamp[4:6]), day=int(timestamp[6:8]), hour=int(timestamp[8:10]), minute=int(timestamp[10:12]), second=int(timestamp[12:14])) 
+		
+		timeobj_3_days = timeobj + datetime.timedelta(days=3)
+		timeobj_30_days = timeobj + datetime.timedelta(days=30)
+		
+		timestamp_3_days = time.strftime("%Y%m%d%H%M%S", timeobj_3_days.timetuple())
+		timestamp_30_days = time.strftime("%Y%m%d%H%M%S", timeobj_30_days.timetuple())
+
 		cursor = self.conn.cursor()
 		cursor.execute("""
 			(
 				SELECT
 					page_namespace as ns,
-					IF(rev_timestamp < %(timestamp)s, "before", "after") as whence,
+					IF(rev_timestamp < %(timestamp)s, "before", IF(rev_timestamp < %(timestamp_3_days)s, "after_0_3", IF(rev_timestamp < %(timestamp_30_days)s, "after_3_30", "after_gt_30"))) as whence,
 					"" as deleted,
 					count(*) as revisions
 				FROM enwiki.revision
@@ -37,7 +55,7 @@ class EditCounts(MetricGenerator):
 			UNION (
 				SELECT
 					ar_namespace as ns,
-					IF(ar_timestamp < %(timestamp)s, "before", "after") as whence,
+					IF(ar_timestamp < %(timestamp)s, "before", IF(ar_timestamp < %(timestamp_3_days)s, "after_0_3", IF(ar_timestamp < %(timestamp_30_days)s, "after_3_30", "after_gt_30"))) as whence,
 					"_deleted" as deleted,
 					count(*) as revisions
 				FROM enwiki.archive
@@ -46,6 +64,8 @@ class EditCounts(MetricGenerator):
 			)""",
 			{
 				'timestamp': timestamp,
+				'timestamp_3_days': timestamp_3_days,
+				'timestamp_30_days': timestamp_30_days,
 				'username': username.encode('utf-8')
 			}
 		)
