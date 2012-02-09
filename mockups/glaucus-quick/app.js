e8dd22168a1434af2d1a006ccdd2808b38f1c552
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2012-02-09 19:39:11 +0000

    show/hide behavior
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@111075 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- mockups/glaucus-quick/app.js
+++ mockups/glaucus-quick/app.js
@@ -6,10 +6,11 @@ $(function() {
 			$active.removeClass('active');
 			$('.menu').hide();
 		}
-	}	
+	}
 	function setupMenu(button, menu) {
 		$(button).bind('click touchstart mousedown', function(e) {
 			e.preventDefault();
+			e.stopPropagation();
 
 			hideMenus();
 			
@@ -21,6 +22,7 @@ $(function() {
 	
 	$('#menu-pane').bind('click touchstart mousedown', function(e) {
 		e.preventDefault();
+		e.stopPropagation();
 		hideMenus();
 		$('#menu-pane').hide();
 	});
@@ -29,6 +31,55 @@ $(function() {
 	setupMenu('#menu-you', '#you-menu');
 	setupMenu('#menu-contribute', '#contribute-menu');
 	setupMenu('#menu-search', '#search-menu');
+
+	function sendHeaderTo(y) {
+		$('header').css('top', y + 'px');
+		$('#menu-pane').css('top', (y + 44) + 'px');
+	}
+	
+	function hideMenuBar() {
+		hideMenus();
+		$('#menu-pane').hide();
+		sendHeaderTo(0);
+	}
+	
+	function showMenuThingy() {
+		var top = $(window).scrollTop(),
+			current = $('header').css('top');
+		if (current == '0px') {
+			sendHeaderTo(top);
+		} else {
+			hideMenuBar();
+		}
+	}
+
+	$(document).bind('click', function() {
+		showMenuThingy();
+	});
+	$(document).bind('scroll', function() {
+		hideMenuBar();
+	});
+
+	$(document).bind('touchstart', function() {
+		var current = $('header').css('top');
+		if (current == '0px') {
+			// simulate click handling for some reason it doesn't work
+			$(document).bind('touchend.menubar', function() {
+				$(document).unbind('touchmove.menubar');
+				$(document).unbind('touchend.menubar');
+				showMenuThingy();
+			});
+			$(document).bind('touchmove.menubar', function() {
+				// moved too much! cancel
+				$(document).unbind('touchmove.menubar');
+				$(document).unbind('touchend.menubar');
+			});
+		} else {
+			// for start of scrolling or touch
+			hideMenuBar();
+		}
+	});
+
 
 	$(window).scrollTop(0);
 });
