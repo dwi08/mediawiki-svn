b3ba890828cf32a3ddc4460ce2b3b02058c5d967
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2012-02-09 18:50:03 +0000

    glaucus mockup: more stub menus; click to dismiss behavior
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@111058 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- mockups/glaucus-quick/app.js
+++ mockups/glaucus-quick/app.js
@@ -1,22 +1,32 @@
 $(function() {
-	
+
+	function hideMenus() {
+		var $active = $('.toolbar button.active');
+		if ($active.length) {
+			$active.removeClass('active');
+			$('.menu').hide();
+		}
+	}	
 	function setupMenu(button, menu) {
 		$(button).bind('click touchstart', function(e) {
 			e.preventDefault();
+
+			hideMenus();
 			
-			var $active = $('.toolbar button.active');
-			if ($active.length) {
-				$active.removeClass('active');
-				$('.menu').hide();
-			} else {
-				$(button).addClass('active');
-				$(menu).show();
-			}
+			$(button).addClass('active');
+			$('#menu-pane').show();
+			$(menu).show();
 		});
 	}
 	
+	$('#menu-pane').bind('click touchstart', function(e) {
+		hideMenus();
+		$('#menu-pane').hide();
+	});
 	setupMenu('#menu-wiki', '#wiki-menu');
 	setupMenu('#menu-article', '#article-menu');
 	setupMenu('#menu-you', '#you-menu');
+	setupMenu('#menu-contribute', '#contribute-menu');
+	setupMenu('#menu-search', '#search-menu');
 
 });
