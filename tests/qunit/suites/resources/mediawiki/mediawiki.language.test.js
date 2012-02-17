394a0cbb763aa4abf2426640a4dac702cd4bd239
santhosh <santhosh@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2012-02-17 06:25:35 +0000

    Port the convertGrammar method of bs and he to js Add qunit test cases for the same
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/jsgrammar@111727 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tests/qunit/suites/resources/mediawiki/mediawiki.language.test.js
+++ tests/qunit/suites/resources/mediawiki/mediawiki.language.test.js
@@ -9,35 +9,53 @@ test( '-- Initial check', function() {
 mw.language.grammartest = function( options ) {
 	var opt = $.extend({
 		language: '',
-		grammarForm: null,
-		word: '',
-		expected: '',
-		description: ''
+		test: [],
 	}, options);
 	// The test works only if the content language is opt.language
 	// because it require [lang].js to be loaded.
 	if( mw.config.get ( 'wgContentLanguage' ) === opt.language ) {
-		test( opt.description, function() {
-			expect( 1 );
-			var langData = mw.language.data;
-			var grammarForms = [];
-			grammarForms[ opt.grammarForm ] = [] ;
-			if ( langData[opt.language] === undefined ) {
-				langData[opt.language] = new mw.Map();
-			}else{
-				grammarForms = langData[opt.language].get( 'grammarForms' );
+		test( "Grammar Test", function() {
+			expect( opt.test.length);
+			for ( var i= 0 ; i < opt.test.length; i++ ) {
+				var langData = mw.language.data;
+				var grammarForms = [];
+				if ( langData[opt.language] === undefined ) {
+					langData[opt.language] = new mw.Map();
+				}else{
+					grammarForms = langData[opt.language].get( 'grammarForms' );
+				}
+				if ( grammarForms[ opt.test[i].grammarForm ] === undefined ) {
+					grammarForms[ opt.test[i].grammarForm ] = [] ;
+				}
+				grammarForms[opt.test[i].grammarForm][opt.test[i].word] = opt.test[i].expected ;
+				langData[opt.language].set( 'grammarForms', grammarForms ); 
+				equal( mw.language.convertGrammar(  opt.test[i].word, opt.test[i].grammarForm ), opt.test[i].expected, opt.test[i].description );
 			}
-			grammarForms[opt.grammarForm][opt.word] = opt.expected ;
-			langData[opt.language].set( 'grammarForms', grammarForms ); 
-			equal( mw.language.convertGrammar(  opt.word, opt.grammarForm ), opt.expected, opt.description );
 		} );
 	}
 }
 
 mw.language.grammartest({
 	language: 'en',
-	word: 'pen',
-	grammarForm: 'genitive',
-	expected: 'pen\'s',
-	description: 'Grammar test for English'
+	test: [ 
+		{ word: 'pen', grammarForm: 'genitive', expected: 'pen\'s', description: 'Grammar test for English' }
+	]
 });
+
+mw.language.grammartest({
+	language: 'bs',
+	test: [
+		{ word: 'word', grammarForm: 'instrumental', expected: 's word', description: 'Grammar test for Bosnian, instrumental case' },
+		{ word: 'word', grammarForm: 'lokativ', expected: 'o word', description: 'Grammar test for Bosnian, lokativ case' }
+	]
+}); 
+
+mw.language.grammartest({
+	language: 'he',
+	test: [
+		{ word: "ויקיפדיה", grammarForm: 'prefixed', expected: "וויקיפדיה", description: 'Grammar test for Hebrew, Duplicate the "Waw" if prefixed' },
+		{ word: "וויקיפדיה", grammarForm: 'prefixed', expected: "וויקיפדיה", description: 'Grammar test for Hebrew, Duplicate the "Waw" if prefixed, but not if it is already duplicated.' },
+		{ word: "הקובץ", grammarForm: 'prefixed', expected: "קובץ", description: 'Grammar test for Hebrew, Remove the "He" if prefixed' },
+		{ word: 'wikipedia', grammarForm: 'תחילית', expected: '־wikipedia', description: 'Grammar test for Hebrew, Add a hyphen (maqaf) if non-Hebrew letters' }
+	]
+}); 
