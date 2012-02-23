3e3b687443b7580b2d268d64b2916c94bd0295c6
oren <oren@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2012-02-23 09:43:22 +0000

    cleanup and fix line delimiter issues
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk@112193 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- lucene-search-3/src/test/java/org/wikimedia/lsearch/analyzers/CJKAnalyzerTest.java
+++ lucene-search-3/src/test/java/org/wikimedia/lsearch/analyzers/CJKAnalyzerTest.java
@@ -1,53 +1,43 @@
 package org.wikimedia.lsearch.analyzers;
 
-import java.io.IOException;
-import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.Token;
-import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.cjk.CJKAnalyzer;
-import org.apache.lucene.queryParser.ParseException;
-import org.apache.lucene.queryParser.QueryParser;
-import org.apache.lucene.search.Query;
+import org.apache.lucene.util.Version;
 import org.junit.Before;
 import org.junit.Test;
-import org.wikimedia.lsearch.analyzers.Aggregate.Flags;
 import org.wikimedia.lsearch.config.Configuration;
 import org.wikimedia.lsearch.config.GlobalConfiguration;
-import org.wikimedia.lsearch.config.IndexId;
-import org.wikimedia.lsearch.ranks.StringList;
 import org.wikimedia.lsearch.test.AbstractWikiTestCase;
 
+/**
+ * tests for the CJKAnalyzer.
+ *
+ * @author oren
+ *
+ */
 public class CJKAnalyzerTest extends AbstractWikiTestCase {
 
-	Analyzer a = null;
-	Configuration config = null;
+    private Analyzer a = null;
+    private Configuration config = null;
 
-	@Before
-	protected void setUp() {
-		super.setUp();
-		if(config == null){
+    /**
+     * setup the fixtures.
+     */
+    @Before
+    public final void setUp() {
+        super.setUp();
+        if (config == null) {
 			config = Configuration.open();
 			GlobalConfiguration.getInstance();
 		}
 	}
 
-	public String tokens(String text){
-		try{
-			return Arrays.toString(tokensFromAnalysis(a,text,"contents"));
-		} catch(IOException e){
-			fail(e.getMessage());
-			return null;
-		}
-	}
-	
+	/**
+	 * tests CJKAnalyzer.
+	 */
 	@Test
-	public void testCJKAnalyzer(){
-		a = new CJKAnalyzer();
+	public final void testCJKAnalyzer() {
+		a = new CJKAnalyzer(Version.LUCENE_24);
 		assertEquals(
 				"[(いわ,0,2,type=double), (わさ,1,3,type=double), (さき,2,4,type=double), (ic,4,6,type=single), (カー,6,8,type=double), (ード,7,9,type=double)]",
 				tokens("いわさきicカード"));
