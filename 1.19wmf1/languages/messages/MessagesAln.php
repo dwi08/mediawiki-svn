9b1aca02ef9e1f5949eac4476954c22efee84ed9
nikerabbit <nikerabbit@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2012-02-27 18:11:46 +0000

    1.19wmf1 MFT r112028 r112186 r111928 r111935 r111403 r111547
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/branches/wmf@112498 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- 1.19wmf1/languages/messages/MessagesAln.php
+++ 1.19wmf1/languages/messages/MessagesAln.php
@@ -43,6 +43,11 @@ $namespaceAliases = array(
 	'Kategori_Diskutim'   => NS_CATEGORY_TALK
 );
 
+$namespaceGenderAliases = array(
+	NS_USER      => array( 'male' => 'Përdoruesi', 'female' => 'Përdoruesja' ),
+	NS_USER_TALK => array( 'male' => 'Përdoruesi_diskutim', 'female' => 'Përdoruesja_diskutim' ),
+);
+
 $specialPageAliases = array(
 	'Popularpages'              => array( 'Faqe të famshme' ),
 	'Search'                    => array( 'Kërko' ),
